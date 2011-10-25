8e4122397e4028a42e487aa875fdc38d0665f260
Peter Donald <peter@realityforge.org>
2011-10-25 17:26:29 +1100

    Rework the TransportConstants type into an interface

--- src/main/java/org/realityforge/replicant/shared/json/TransportConstants.java
+++ src/main/java/org/realityforge/replicant/shared/json/TransportConstants.java
@@ -3,11 +3,11 @@ package org.realityforge.replicant.shared.json;
 /**
  * Constants used to build up JSON payload transmitted to the client.
  */
-public final class TransportConstants
+public interface TransportConstants
 {
-  public static final String LAST_CHANGE_SET_ID = "last_id";
-  public static final String CHANGES = "changes";
-  public static final String ENTITY_ID = "id";
-  public static final String TYPE_ID = "type";
-  public static final String DATA = "data";
+  String LAST_CHANGE_SET_ID = "last_id";
+  String CHANGES = "changes";
+  String ENTITY_ID = "id";
+  String TYPE_ID = "type";
+  String DATA = "data";
 }
