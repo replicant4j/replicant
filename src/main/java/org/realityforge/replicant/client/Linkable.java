e1d584e1b6f9ab08a681d350e29dfcb9646340fa
Peter Donald <peter@realityforge.org>
2012-02-28 10:01:34 +1100

    Update the documentation to document that Linkable is the contract between the EntityRepository and the entity

--- src/main/java/org/realityforge/replicant/client/Linkable.java
+++ src/main/java/org/realityforge/replicant/client/Linkable.java
@@ -1,7 +1,9 @@
 package org.realityforge.replicant.client;
 
 /**
- * Interface implemented by imitations that need to be linked.
+ * Interface implemented by imitations that need to be linked. This is a contract between an entity and the
+ * entity repository. Note: the methods on this interface should only be invoked by the repository and not by users
+ * of the entity repository.
  */
 public interface Linkable
 {
