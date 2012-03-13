302f87d1361938fb3ee536a213209245c5c801e1
Peter Donald <peter@realityforge.org>
2012-03-13 14:14:19 +1100

    Add in wrappers for primitive type arrays to structured java collections

---
+++ src/main/java/org/realityforge/replicant/client/json/gwt/JsoReadOnlyIntegerSet.java
@@ -0,0 +1,63 @@
+package org.realityforge.replicant.client.json.gwt;
+
+import com.google.gwt.core.client.JsArrayInteger;
+import java.util.AbstractSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * An adapter converting the JsArrayString to support a Set interface.
+ */
+public final class JsoReadOnlyIntegerSet
+  extends AbstractSet<Integer>
+  implements Set<Integer>
+{
+  private final JsArrayInteger _data;
+
+  public JsoReadOnlyIntegerSet( final JsArrayInteger data )
+  {
+    _data = data;
+  }
+
+  @Override
+  public Iterator<Integer> iterator()
+  {
+    return new JsoIterator( this );
+  }
+
+  @Override
+  public int size()
+  {
+    return _data.length();
+  }
+
+  static class JsoIterator
+    implements Iterator<Integer>
+  {
+    private JsoReadOnlyIntegerSet _set;
+    private int _offset;
+
+    JsoIterator( final JsoReadOnlyIntegerSet set )
+    {
+      _set = set;
+    }
+
+    @Override
+    public boolean hasNext()
+    {
+      return _offset < _set.size();
+    }
+
+    @Override
+    public Integer next()
+    {
+      return _set._data.get( _offset++ );
+    }
+
+    @Override
+    public void remove()
+    {
+      throw new UnsupportedOperationException();
+    }
+  }
+}
---
+++ src/main/java/org/realityforge/replicant/client/json/gwt/JsoReadOnlyStringList.java
@@ -0,0 +1,32 @@
+package org.realityforge.replicant.client.json.gwt;
+
+import com.google.gwt.core.client.JsArrayString;
+import java.util.AbstractList;
+import java.util.List;
+
+/**
+ * An adapter converting the JsArrayString to support a List interface.
+ */
+public final class JsoReadOnlyStringList
+  extends AbstractList<String>
+  implements List<String>
+{
+  private final JsArrayString _data;
+
+  public JsoReadOnlyStringList( final JsArrayString data )
+  {
+    _data = data;
+  }
+
+  @Override
+  public String get( final int index )
+  {
+    return _data.get( index );
+  }
+
+  @Override
+  public int size()
+  {
+    return _data.length();
+  }
+}
