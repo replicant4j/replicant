9cb8f7063ff85b02511efab5bcfdd4db478b8156
Peter Donald <peter@realityforge.org>
2013-07-15 08:15:34 +1000

    Optimise import

--- src/main/java/org/realityforge/replicant/server/json/jackson/DateSetDeserializer.java
+++ src/main/java/org/realityforge/replicant/server/json/jackson/DateSetDeserializer.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import org.codehaus.jackson.JsonParser;
 import org.codehaus.jackson.JsonToken;
