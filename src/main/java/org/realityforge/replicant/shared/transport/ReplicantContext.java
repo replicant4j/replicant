89eded8175bf3de6bc5b4499e3e65ac2251296af
Peter Donald <peter@realityforge.org>
2014-01-31 10:01:14 +1100

    Add some javadocs

--- src/main/java/org/realityforge/replicant/shared/transport/ReplicantContext.java
+++ src/main/java/org/realityforge/replicant/shared/transport/ReplicantContext.java
@@ -2,11 +2,32 @@ package org.realityforge.replicant.shared.transport;
 
 public class ReplicantContext
 {
+  /**
+   * Key used to retrieve an opaque identifier for the session from the ReplicantContextHolder.
+   * Used to pass data from the servlet to the EJB.
+   */
   public static final String SESSION_ID_KEY = "SessionID";
+  /**
+   * Key used to retrieve an opaque identifier for the request from the ReplicantContextHolder.
+   * Used to pass data from the servlet to the EJB.
+   */
   public static final String REQUEST_ID_KEY = "RequestID";
+  /**
+   * Key used to retrieve a flag whether the request produced a changeset relevant for the initiating session..
+   * Used to pass data from the EJB to the servlet.
+   */
   public static final String REQUEST_COMPLETE_KEY = "RequestComplete";
 
+  /**
+   * HTTP request header to indicate the request id.
+   */
   public static final String REQUEST_ID_HEADER = "X-GWT-RequestID";
+  /**
+   * HTTP response header to indicate the whether the request is complete or a change set is expected.
+   */
   public static final String REQUEST_COMPLETE_HEADER = "X-GWT-RequestComplete";
+  /**
+   * HTTP request header to indicate the session id.
+   */
   public static final String SESSION_ID_HEADER = "X-GWT-SessionID";
 }
