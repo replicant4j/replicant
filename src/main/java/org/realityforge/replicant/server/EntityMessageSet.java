4f220de7de407b1e271d5f3da4dba82a89d46f3f
Peter Donald <peter@realityforge.org>
2014-02-05 13:11:14 +1100

    Support different variants of merging for entity sets, including merging collections

--- src/main/java/org/realityforge/replicant/server/EntityMessageSet.java
+++ src/main/java/org/realityforge/replicant/server/EntityMessageSet.java
@@ -1,23 +1,57 @@
 package org.realityforge.replicant.server;
 
+import java.io.Serializable;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 public class EntityMessageSet
 {
   private final LinkedHashMap<String, EntityMessage> _entities = new LinkedHashMap<>();
 
+  public void mergeAll( final Collection<EntityMessage> messages )
+  {
+    mergeAll( messages, false );
+  }
+
+  public void mergeAll( final Collection<EntityMessage> messages, final boolean copyOnMerge )
+  {
+    for ( final EntityMessage message : messages )
+    {
+      merge( message, copyOnMerge );
+    }
+  }
+
   public void merge( final EntityMessage message )
+  {
+    merge( message, false );
+  }
+
+  public void merge( final EntityMessage message, final boolean copyOnMerge )
   {
     final String key = message.getTypeID() + "#" + message.getID();
     final EntityMessage existing = _entities.get( key );
-    if( null != existing )
+    if ( null != existing )
     {
       existing.merge( message );
     }
     else
     {
-      _entities.put( key, message );
+      final EntityMessage messageToInsert;
+      if ( copyOnMerge )
+      {
+        messageToInsert = new EntityMessage( message.getID(),
+                                             message.getTypeID(),
+                                             message.getTimestamp(),
+                                             new HashMap<String, Serializable>(),
+                                             new HashMap<String, Serializable>() );
+        messageToInsert.merge( message );
+      }
+      else
+      {
+        messageToInsert = message;
+      }
+      _entities.put( key, messageToInsert );
     }
   }
 
