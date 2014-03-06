175bad2d39fd5d2bf3ee5f252fa661cb9e4d06ac
Peter Donald <peter@realityforge.org>
2014-03-06 20:17:53 +1100

    Rework ClientSession now that more functionality has moved to it

--- src/main/java/org/realityforge/replicant/client/transport/ClientSession.java
+++ src/main/java/org/realityforge/replicant/client/transport/ClientSession.java
@@ -6,31 +6,39 @@ import java.util.LinkedList;
 import java.util.Map;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import org.realityforge.replicant.client.transport.AreaOfInterestAction.Action;
 
 /**
  * Client-side representation of session.
  * Includes a database of outstanding requests, a subscription database and a
  * collection of outstanding data actions to apply.
  *
- * @param <T> The enum type representing the different graphs.
+ * @param <G> The enum type representing the different graphs.
  */
-public abstract class ClientSession<T extends Enum>
+public abstract class ClientSession<T extends ClientSession<T,G>, G extends Enum>
 {
+  @Nonnull
+  private final AbstractDataLoaderService<T, G> _dataLoaderService;
   private final String _sessionID;
   private final Map<String, RequestEntry> _requests = new HashMap<String, RequestEntry>();
   private final Map<String, RequestEntry> _roRequests = Collections.unmodifiableMap( _requests );
   private int _requestID;
 
   //Graph => InstanceID
-  private final HashMap<T, Map<Object, SubscriptionEntry<T>>> _instanceSubscriptions =
-    new HashMap<T, Map<Object, SubscriptionEntry<T>>>();
-  private final Map<T, Map<Object, SubscriptionEntry<T>>> _roInstanceSubscriptions =
+  private final HashMap<G, Map<Object, SubscriptionEntry<G>>> _instanceSubscriptions =
+    new HashMap<G, Map<Object, SubscriptionEntry<G>>>();
+  private final Map<G, Map<Object, SubscriptionEntry<G>>> _roInstanceSubscriptions =
     Collections.unmodifiableMap( _instanceSubscriptions );
 
   //Graph => Type
-  private final HashMap<T, SubscriptionEntry<T>> _typeSubscriptions = new HashMap<T, SubscriptionEntry<T>>();
-  private final Map<T, SubscriptionEntry<T>> _roTypeSubscriptions = Collections.unmodifiableMap( _typeSubscriptions );
+  private final HashMap<G, SubscriptionEntry<G>> _typeSubscriptions = new HashMap<G, SubscriptionEntry<G>>();
+  private final Map<G, SubscriptionEntry<G>> _roTypeSubscriptions = Collections.unmodifiableMap( _typeSubscriptions );
 
+  /**
+   * Pending actions that will change the area of interest.
+   */
+  private final LinkedList<AreaOfInterestAction<G>> _pendingAreaOfInterestActions =
+    new LinkedList<AreaOfInterestAction<G>>();
   /**
    * The set of data load actions that still need to have the json parsed.
    */
@@ -48,8 +56,10 @@ public abstract class ClientSession<T extends Enum>
 
   private int _lastRxSequence;
 
-  public ClientSession( @Nonnull final String sessionID )
+  protected ClientSession( @Nonnull final AbstractDataLoaderService<T, G> dataLoaderService,
+                           @Nonnull final String sessionID )
   {
+    _dataLoaderService = dataLoaderService;
     _sessionID = sessionID;
   }
 
@@ -57,6 +67,74 @@ public abstract class ClientSession<T extends Enum>
   public String getSessionID()
   {
     return _sessionID;
+  }
+
+  protected void subscribe( @Nonnull final G graph,
+                            @Nullable final Object id,
+                            @Nonnull final String cacheKey,
+                            @Nullable final Object filterParameter,
+                            @Nullable final Runnable userAction )
+  {
+    enqueueAoiAction( graph, Action.ADD, cacheKey, id, filterParameter, userAction );
+  }
+
+  protected void updateSubscription( @Nonnull final G graph,
+                                     @Nullable final Object id,
+                                     @Nullable final Object filterParameter,
+                                     @Nullable final Runnable userAction )
+  {
+    enqueueAoiAction( graph, Action.UPDATE, null, id, filterParameter, userAction );
+  }
+
+  protected void unsubscribe( @Nonnull final G graph,
+                              @Nullable final Object id,
+                              @Nullable final Runnable userAction )
+  {
+    enqueueAoiAction( graph, Action.REMOVE, null, id, null, userAction );
+  }
+
+  private void enqueueAoiAction( @Nonnull final G graph,
+                                 @Nonnull final Action action,
+                                 @Nullable final String cacheKey,
+                                 @Nullable final Object id,
+                                 @Nullable final Object filterParameter,
+                                 @Nullable final Runnable userAction )
+  {
+    _pendingAreaOfInterestActions.
+      add( new AreaOfInterestAction<G>( graph, action, cacheKey, id, filterParameter, userAction ) );
+    _dataLoaderService.scheduleDataLoad();
+  }
+
+  @SuppressWarnings( "ConstantConditions" )
+  public final void enqueueDataLoad( @Nonnull final String rawJsonData )
+  {
+    if ( null == rawJsonData )
+    {
+      throw new IllegalStateException( "null == rawJsonData" );
+    }
+    getPendingActions().add( new DataLoadAction( rawJsonData, false ) );
+    _dataLoaderService.scheduleDataLoad();
+  }
+
+  @SuppressWarnings( "ConstantConditions" )
+  protected final void enqueueOOB( @Nonnull final String rawJsonData,
+                                   @Nullable final Runnable runnable,
+                                   final boolean bulkLoad )
+  {
+    if ( null == rawJsonData )
+    {
+      throw new IllegalStateException( "null == rawJsonData" );
+    }
+    final DataLoadAction action = new DataLoadAction( rawJsonData, true );
+    action.setRunnable( runnable );
+    action.setBulkLoad( bulkLoad );
+    getOobActions().add( action );
+    _dataLoaderService.scheduleDataLoad();
+  }
+
+  final LinkedList<AreaOfInterestAction<G>> getPendingAreaOfInterestActions()
+  {
+    return _pendingAreaOfInterestActions;
   }
 
   final LinkedList<DataLoadAction> getPendingActions()
@@ -85,7 +163,9 @@ public abstract class ClientSession<T extends Enum>
   }
 
   @Nonnull
-  public final RequestEntry newRequestRegistration( @Nonnull final String requestKey, @Nullable final String cacheKey, final boolean bulkLoad )
+  public final RequestEntry newRequestRegistration( @Nonnull final String requestKey,
+                                                    @Nullable final String cacheKey,
+                                                    final boolean bulkLoad )
   {
     final RequestEntry entry = new RequestEntry( newRequestID(), requestKey, cacheKey, bulkLoad );
     _requests.put( entry.getRequestID(), entry );
@@ -117,7 +197,7 @@ public abstract class ClientSession<T extends Enum>
   /**
    * @return read-only map of instance subscriptions.
    */
-  protected final Map<T, Map<Object, SubscriptionEntry<T>>> getInstanceSubscriptions()
+  protected final Map<G, Map<Object, SubscriptionEntry<G>>> getInstanceSubscriptions()
   {
     return _roInstanceSubscriptions;
   }
@@ -125,7 +205,7 @@ public abstract class ClientSession<T extends Enum>
   /**
    * @return read-only map of type subscriptions.
    */
-  protected final Map<T, SubscriptionEntry<T>> getTypeSubscriptions()
+  protected final Map<G, SubscriptionEntry<G>> getTypeSubscriptions()
   {
     return _roTypeSubscriptions;
   }
@@ -137,12 +217,12 @@ public abstract class ClientSession<T extends Enum>
    * @return the subscription entry if this was the first subscription for graph, null otherwise.
    */
   @Nullable
-  protected final SubscriptionEntry<T> subscribeToTypeGraph( @Nonnull final T graph )
+  final SubscriptionEntry<G> newTypeGraphSubscription( @Nonnull final G graph )
   {
-    SubscriptionEntry<T> typeMap = findTypeGraphSubscription( graph );
+    SubscriptionEntry<G> typeMap = findTypeGraphSubscription( graph );
     if ( null == typeMap )
     {
-      final SubscriptionEntry<T> entry = new SubscriptionEntry<T>( graph, null );
+      final SubscriptionEntry<G> entry = new SubscriptionEntry<G>( graph, null );
       _typeSubscriptions.put( graph, entry );
       return entry;
     }
@@ -159,7 +239,7 @@ public abstract class ClientSession<T extends Enum>
    * @return the subscription entry if it exists, null otherwise.
    */
   @Nullable
-  protected final SubscriptionEntry<T> findTypeGraphSubscription( @Nonnull final T graph )
+  protected final SubscriptionEntry<G> findTypeGraphSubscription( @Nonnull final G graph )
   {
     return _typeSubscriptions.get( graph );
   }
@@ -172,17 +252,17 @@ public abstract class ClientSession<T extends Enum>
    * @return the subscription entry if this was the first subscription for graph, null otherwise.
    */
   @Nullable
-  protected final SubscriptionEntry<T> subscribeToInstanceGraph( @Nonnull final T graph, @Nonnull final Object id )
+  final SubscriptionEntry<G> newInstanceGraphSubscription( @Nonnull final G graph, @Nonnull final Object id )
   {
-    Map<Object, SubscriptionEntry<T>> instanceMap = _instanceSubscriptions.get( graph );
+    Map<Object, SubscriptionEntry<G>> instanceMap = _instanceSubscriptions.get( graph );
     if ( null == instanceMap )
     {
-      instanceMap = new HashMap<Object, SubscriptionEntry<T>>();
+      instanceMap = new HashMap<Object, SubscriptionEntry<G>>();
       _instanceSubscriptions.put( graph, instanceMap );
     }
     if ( !instanceMap.containsKey( id ) )
     {
-      final SubscriptionEntry<T> entry = new SubscriptionEntry<T>( graph, id );
+      final SubscriptionEntry<G> entry = new SubscriptionEntry<G>( graph, id );
       instanceMap.put( id, entry );
       return entry;
     }
@@ -200,9 +280,10 @@ public abstract class ClientSession<T extends Enum>
    * @return the subscription entry if it exists, null otherwise.
    */
   @Nullable
-  protected final SubscriptionEntry<T> findInstanceGraphSubscription( @Nonnull final T graph, @Nonnull final Object id )
+  protected final SubscriptionEntry<G> findInstanceGraphSubscription( @Nonnull final G graph,
+                                                                      @Nonnull final Object id )
   {
-    Map<Object, SubscriptionEntry<T>> instanceMap = _instanceSubscriptions.get( graph );
+    Map<Object, SubscriptionEntry<G>> instanceMap = _instanceSubscriptions.get( graph );
     if ( null == instanceMap )
     {
       return null;
@@ -220,9 +301,9 @@ public abstract class ClientSession<T extends Enum>
    * @return the subscription entry if subscribed, null otherwise.
    */
   @Nullable
-  protected final SubscriptionEntry<T> unsubscribeFromTypeGraph( @Nonnull final T graph )
+  final SubscriptionEntry<G> unsubscribeFromTypeGraph( @Nonnull final G graph )
   {
-    final SubscriptionEntry<T> entry = _typeSubscriptions.remove( graph );
+    final SubscriptionEntry<G> entry = _typeSubscriptions.remove( graph );
     if ( null != entry )
     {
       entry.markDeregisterInProgress();
@@ -241,14 +322,14 @@ public abstract class ClientSession<T extends Enum>
    * @param id    the id of the root object.
    * @return the subscription entry if subscribed, null otherwise.
    */
-  protected final SubscriptionEntry<T> unsubscribeFromInstanceGraph( @Nonnull final T graph, @Nonnull final Object id )
+  final SubscriptionEntry<G> unsubscribeFromInstanceGraph( @Nonnull final G graph, @Nonnull final Object id )
   {
-    final Map<Object, SubscriptionEntry<T>> instanceMap = _instanceSubscriptions.get( graph );
+    final Map<Object, SubscriptionEntry<G>> instanceMap = _instanceSubscriptions.get( graph );
     if ( null == instanceMap )
     {
       return null;
     }
-    final SubscriptionEntry<T> entry = instanceMap.remove( id );
+    final SubscriptionEntry<G> entry = instanceMap.remove( id );
     if ( null != entry )
     {
       entry.markDeregisterInProgress();
