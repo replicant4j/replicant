345383dbfe8969b270dc69022811fcb167e64d67
Peter Donald <peter@realityforge.org>
2014-03-28 08:54:57 +1100

    A simple representation of a link between an entity and other channels

---
+++ src/main/java/org/realityforge/replicant/server/ChannelLink.java
@@ -0,0 +1,45 @@
+package org.realityforge.replicant.server;
+
+import javax.annotation.Nonnull;
+
+/**
+ * A representation indicating that an entity message will cause another channel to be subscribed.
+ */
+public final class ChannelLink
+{
+  @Nonnull
+  private final ChannelDescriptor _targetChannel;
+
+  public ChannelLink( @Nonnull final ChannelDescriptor targetChannel )
+  {
+    _targetChannel = targetChannel;
+  }
+
+  @Nonnull
+  public ChannelDescriptor getTargetChannel()
+  {
+    return _targetChannel;
+  }
+
+  @Override
+  public boolean equals( final Object o )
+  {
+    if ( this == o )
+    {
+      return true;
+    }
+    if ( o == null || getClass() != o.getClass() )
+    {
+      return false;
+    }
+
+    final ChannelLink that = (ChannelLink) o;
+    return _targetChannel.equals( that._targetChannel );
+  }
+
+  @Override
+  public int hashCode()
+  {
+    return _targetChannel.hashCode();
+  }
+}
