b41bafadaf4a83b4ef2351d6f3e0dc5e67b379a8
Peter Donald <peter@realityforge.org>
2014-03-28 09:05:37 +1100

    Remove all the infrastructure for multiple EntityMessage instances for an entity as we will use links instead

--- src/main/java/org/realityforge/replicant/server/ee/ChangeRecorder.java
+++ src/main/java/org/realityforge/replicant/server/ee/ChangeRecorder.java
@@ -1,8 +1,5 @@
 package org.realityforge.replicant.server.ee;
 
-import java.io.Serializable;
-import java.util.Collections;
-import java.util.Map;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.Resource;
@@ -56,7 +53,7 @@ public abstract class ChangeRecorder
   {
     if ( !inRollback() )
     {
-      recordEntityMessagesForEntity( object, true );
+      recordEntityMessageForEntity( object, true );
     }
   }
 
@@ -74,7 +71,7 @@ public abstract class ChangeRecorder
   {
     if ( !inRollback() )
     {
-      recordEntityMessagesForEntity( object, false );
+      recordEntityMessageForEntity( object, false );
     }
   }
 
@@ -87,34 +84,6 @@ public abstract class ChangeRecorder
   }
 
   /**
-   * Record the EntityMessage for the specified entity and dependent entities in the transactions EntityMessageSet.
-   *
-   * @param entity   the entity to record.
-   * @param isUpdate true if change is an update, false if it is a delete.
-   * @return the EntityMessage for specified entity if any was recorded
-   */
-  @Nullable
-  protected final EntityMessage recordEntityMessagesForEntity( @Nonnull final Object entity, final boolean isUpdate )
-  {
-    return recordEntityMessagesForEntity( entity, isUpdate, Collections.<String, Serializable>emptyMap() );
-  }
-
-  /**
-   * Record the EntityMessage for the specified entity and dependent entities in the transactions EntityMessageSet.
-   *
-   * @param entity   the entity to record.
-   * @param isUpdate true if change is an update, false if it is a delete.
-   * @return the EntityMessage for specified entity if any was recorded
-   */
-  @Nullable
-  protected EntityMessage recordEntityMessagesForEntity( @Nonnull final Object entity,
-                                                         final boolean isUpdate,
-                                                         @Nonnull final Map<String, Serializable> extraRoutingKeys )
-  {
-    return recordEntityMessageForEntity( entity, isUpdate, extraRoutingKeys );
-  }
-
-  /**
    * Record the EntityMessage for specified entity in the transactions EntityMessageSet.
    *
    * @param entity   the entity to record.
@@ -122,14 +91,11 @@ public abstract class ChangeRecorder
    * @return the EntityMessage for specified entity if any was recorded
    */
   @Nullable
-  protected final EntityMessage recordEntityMessageForEntity( @Nonnull final Object entity,
-                                                              final boolean isUpdate,
-                                                              @Nonnull final Map<String, Serializable> extraRoutingKeys )
+  protected final EntityMessage recordEntityMessageForEntity( @Nonnull final Object entity, final boolean isUpdate )
   {
     final EntityMessage entityMessage = getEntityMessageGenerator().convertToEntityMessage( entity, isUpdate );
     if ( null != entityMessage )
     {
-      entityMessage.getRoutingKeys().putAll( extraRoutingKeys );
       getEntityMessageSet().merge( entityMessage );
     }
     return entityMessage;
