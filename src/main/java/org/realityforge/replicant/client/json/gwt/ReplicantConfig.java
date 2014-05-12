d73367557183f18f2797964dcad7ce07ce2ae0c6
Peter Donald <peter@realityforge.org>
2014-05-12 16:21:44 +1000

    Remove unnecessary annotation as it matches the method name

--- src/main/java/org/realityforge/replicant/client/json/gwt/ReplicantConfig.java
+++ src/main/java/org/realityforge/replicant/client/json/gwt/ReplicantConfig.java
@@ -2,12 +2,10 @@ package org.realityforge.replicant.client.json.gwt;
 
 import org.realityforge.gwt.propertysource.client.PropertySource;
 import org.realityforge.gwt.propertysource.client.annotations.Namespace;
-import org.realityforge.gwt.propertysource.client.annotations.Property;
 
 @Namespace("replicant")
 public interface ReplicantConfig
   extends PropertySource
 {
-  @Property( "shouldValidateRepositoryOnLoad" )
   boolean shouldValidateRepositoryOnLoad();
 }
