b863daf5d0d9bac06f2c04afeb3552da703e3ce4
Peter Donald <peter@realityforge.org>
2014-06-20 16:39:07 +1000

    Make sure subscription entry can hold the filter value

--- src/main/java/org/realityforge/replicant/client/GraphSubscriptionEntry.java
+++ src/main/java/org/realityforge/replicant/client/GraphSubscriptionEntry.java
@@ -3,29 +3,48 @@ package org.realityforge.replicant.client;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 /**
  * Representation of a subscription to a graph.
  */
-public class GraphSubscriptionEntry
+public final class GraphSubscriptionEntry
 {
   private final GraphDescriptor _descriptor;
-
   @Nullable
+  private Object _filter;
+
   private final Map<Class<?>, Map<Object, EntitySubscriptionEntry>> _entities =
     new HashMap<Class<?>, Map<Object, EntitySubscriptionEntry>>();
   private final Map<Class<?>, Map<Object, EntitySubscriptionEntry>> _roEntities =
     Collections.unmodifiableMap( _entities );
 
-  public GraphSubscriptionEntry( final GraphDescriptor descriptor )
+  public GraphSubscriptionEntry( @Nonnull final GraphDescriptor descriptor,
+                                 @Nullable final Object filter )
   {
     _descriptor = descriptor;
+    _filter = filter;
+  }
+
+  public GraphSubscriptionEntry( final GraphDescriptor descriptor )
+  {
+    this( descriptor, null );
   }
 
   public GraphDescriptor getDescriptor()
   {
     return _descriptor;
+  }
+
+  void setFilter( final Object filter )
+  {
+    _filter = filter;
+  }
+
+  public Object getFilter()
+  {
+    return _filter;
   }
 
   public Map<Class<?>, Map<Object, EntitySubscriptionEntry>> getEntities()
