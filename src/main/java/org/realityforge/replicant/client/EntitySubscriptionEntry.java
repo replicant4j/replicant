67a4c416e38ee2f72cd0f951a111186a26d9e1a6
Peter Donald <peter@realityforge.org>
2014-06-23 20:11:15 +1000

    Correct the nullability annotations

--- src/main/java/org/realityforge/replicant/client/EntitySubscriptionEntry.java
+++ src/main/java/org/realityforge/replicant/client/EntitySubscriptionEntry.java
@@ -3,35 +3,33 @@ package org.realityforge.replicant.client;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import javax.annotation.Nullable;
+import javax.annotation.Nonnull;
 
 /**
  * Representation of a subscriptions that impact entity.
  */
 public class EntitySubscriptionEntry
 {
-  private final Class<?> _type;
-  @Nullable
-  private final Object _id;
-
-  @Nullable
   private final Map<GraphDescriptor, GraphSubscriptionEntry> _graphSubscriptions =
     new HashMap<GraphDescriptor, GraphSubscriptionEntry>();
   private final Map<GraphDescriptor, GraphSubscriptionEntry> _roGraphSubscriptions =
     Collections.unmodifiableMap( _graphSubscriptions );
+  private final Class<?> _type;
+  private final Object _id;
 
-  public EntitySubscriptionEntry( final Class<?> type, final Object id )
+  public EntitySubscriptionEntry( @Nonnull final Class<?> type, @Nonnull final Object id )
   {
     _type = type;
     _id = id;
   }
 
+  @Nonnull
   public Class<?> getType()
   {
     return _type;
   }
 
-  @Nullable
+  @Nonnull
   public Object getID()
   {
     return _id;
