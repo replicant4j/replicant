3d909ede94387d3248500f3f0a58455ed295a99e
Peter Donald <peter@realityforge.org>
2014-06-24 15:26:29 +1000

    Use diamond operator where possible

--- src/test/java/org/realityforge/replicant/client/transport/DataLoaderServiceTest.java
+++ src/test/java/org/realityforge/replicant/client/transport/DataLoaderServiceTest.java
@@ -509,7 +509,7 @@ public class DataLoaderServiceTest
                                                            final int expectedStepCount,
                                                            final int expectedActions )
   {
-    final LinkedList<DataLoadAction> actionsProcessed = new LinkedList<DataLoadAction>();
+    final LinkedList<DataLoadAction> actionsProcessed = new LinkedList<>();
     int count = 0;
     while ( true )
     {
