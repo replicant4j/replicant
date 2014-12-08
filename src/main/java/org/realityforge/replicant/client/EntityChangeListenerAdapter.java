25e7d6a777865f9dba64e0631d29f0a7529a0d1e
Peter Donald <peter@realityforge.org>
2014-12-08 17:29:47 +1100

    Add empty adapter to make writing change listeners easier

---
+++ src/main/java/org/realityforge/replicant/client/EntityChangeListenerAdapter.java
@@ -0,0 +1,32 @@
+package org.realityforge.replicant.client;
+
+import javax.annotation.Nonnull;
+
+public class EntityChangeListenerAdapter
+  implements EntityChangeListener
+{
+  @Override
+  public void entityAdded( @Nonnull final EntityChangeEvent event )
+  {
+  }
+
+  @Override
+  public void entityRemoved( @Nonnull final EntityChangeEvent event )
+  {
+  }
+
+  @Override
+  public void attributeChanged( @Nonnull final EntityChangeEvent event )
+  {
+  }
+
+  @Override
+  public void relatedAdded( @Nonnull final EntityChangeEvent event )
+  {
+  }
+
+  @Override
+  public void relatedRemoved( @Nonnull final EntityChangeEvent event )
+  {
+  }
+}
