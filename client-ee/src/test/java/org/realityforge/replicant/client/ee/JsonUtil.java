9d6b7025bd62f44c2d77600465dd24935ca5f574
Peter Donald <peter@realityforge.org>
2016-12-16 08:07:02 +1100

    Ensure jsonReader is closed

--- client-ee/src/test/java/org/realityforge/replicant/client/ee/JsonUtil.java
+++ client-ee/src/test/java/org/realityforge/replicant/client/ee/JsonUtil.java
@@ -4,9 +4,13 @@ import java.io.StringReader;
 import javax.annotation.Nonnull;
 import javax.json.Json;
 import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.JsonReaderFactory;
 
 public final class JsonUtil
 {
+  private static final JsonReaderFactory c_readerFactory = Json.createReaderFactory( null );
+
   private JsonUtil()
   {
   }
@@ -14,6 +18,10 @@ public final class JsonUtil
   @Nonnull
   static JsonObject toJsonObject( @Nonnull final String content )
   {
-    return Json.createReader( new StringReader( content ) ).readObject();
+    final StringReader stringReader = new StringReader( content );
+    try ( final JsonReader reader = c_readerFactory.createReader( stringReader ) )
+    {
+      return reader.readObject();
+    }
   }
 }
