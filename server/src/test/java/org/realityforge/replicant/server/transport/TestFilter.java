d90c9afc45b3b0c03f8b28e88eecab8d5d91c27d
Peter Donald <peter@realityforge.org>
2016-12-19 10:59:21 +1100

    Make class serializable to make testing easier

--- server/src/test/java/org/realityforge/replicant/server/transport/TestFilter.java
+++ server/src/test/java/org/realityforge/replicant/server/transport/TestFilter.java
@@ -2,10 +2,12 @@ package org.realityforge.replicant.server.transport;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonTypeName;
+import java.io.Serializable;
 
 @JsonAutoDetect( fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE, isGetterVisibility = JsonAutoDetect.Visibility.NONE, creatorVisibility = JsonAutoDetect.Visibility.NONE )
 @JsonTypeName( "testFilter" )
 public class TestFilter
+  implements Serializable
 {
   private int myField;
 
