16963206f95881dec56289bba9d6402592e825fb
Peter Donald <peter@realityforge.org>
2017-04-07 10:55:10 +1000

    Introduce DataLoaderListenerSupport to help propagate events to multiple listeners

---
+++ client-common/src/main/java/org/realityforge/replicant/client/transport/DataLoaderListenerSupport.java
@@ -0,0 +1,137 @@
+package org.realityforge.replicant.client.transport;
+
+import java.util.ArrayList;
+import javax.annotation.Nonnull;
+import org.realityforge.replicant.client.ChannelDescriptor;
+
+public final class DataLoaderListenerSupport
+  implements DataLoaderListener
+{
+  private final ArrayList<DataLoaderListener> _listeners = new ArrayList<>();
+
+  public boolean addListener( @Nonnull final DataLoaderListener listener )
+  {
+    if ( !_listeners.contains( listener ) )
+    {
+      _listeners.add( listener );
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+  public boolean removeListener( @Nonnull final DataLoaderListener listener )
+  {
+    return _listeners.remove( listener );
+  }
+
+  @Override
+  public void onDisconnect( @Nonnull final DataLoaderService service )
+  {
+    _listeners.forEach( l -> l.onDisconnect( service ) );
+  }
+
+  @Override
+  public void onInvalidDisconnect( @Nonnull final DataLoaderService service, @Nonnull final Throwable throwable )
+  {
+    _listeners.forEach( l -> l.onInvalidDisconnect( service, throwable ) );
+  }
+
+  @Override
+  public void onConnect( @Nonnull final DataLoaderService service )
+  {
+    _listeners.forEach( l -> l.onConnect( service ) );
+  }
+
+  @Override
+  public void onInvalidConnect( @Nonnull final DataLoaderService service, @Nonnull final Throwable throwable )
+  {
+    _listeners.forEach( l -> l.onInvalidConnect( service, throwable ) );
+  }
+
+  @Override
+  public void onDataLoadComplete( @Nonnull final DataLoaderService service, @Nonnull final DataLoadStatus status )
+  {
+    _listeners.forEach( l -> l.onDataLoadComplete( service, status ) );
+  }
+
+  @Override
+  public void onDataLoadFailure( @Nonnull final DataLoaderService service, @Nonnull final Throwable throwable )
+  {
+    _listeners.forEach( l -> l.onDataLoadFailure( service, throwable ) );
+  }
+
+  @Override
+  public void onPollFailure( @Nonnull final DataLoaderService service, @Nonnull final Throwable throwable )
+  {
+    _listeners.forEach( l -> l.onPollFailure( service, throwable ) );
+  }
+
+  @Override
+  public void onSubscribeStarted( @Nonnull final DataLoaderService service,
+                                  @Nonnull final ChannelDescriptor descriptor )
+  {
+    _listeners.forEach( l -> l.onSubscribeStarted( service, descriptor ) );
+  }
+
+  @Override
+  public void onSubscribeCompleted( @Nonnull final DataLoaderService service,
+                                    @Nonnull final ChannelDescriptor descriptor )
+  {
+    _listeners.forEach( l -> l.onSubscribeCompleted( service, descriptor ) );
+  }
+
+  @Override
+  public void onSubscribeFailed( @Nonnull final DataLoaderService service,
+                                 @Nonnull final ChannelDescriptor descriptor,
+                                 @Nonnull final Throwable throwable )
+  {
+    _listeners.forEach( l -> l.onSubscribeFailed( service, descriptor, throwable ) );
+  }
+
+  @Override
+  public void onUnsubscribeStarted( @Nonnull final DataLoaderService service,
+                                    @Nonnull final ChannelDescriptor descriptor )
+  {
+    _listeners.forEach( l -> l.onUnsubscribeStarted( service, descriptor ) );
+  }
+
+  @Override
+  public void onUnsubscribeCompleted( @Nonnull final DataLoaderService service,
+                                      @Nonnull final ChannelDescriptor descriptor )
+  {
+    _listeners.forEach( l -> l.onUnsubscribeCompleted( service, descriptor ) );
+  }
+
+  @Override
+  public void onUnsubscribeFailed( @Nonnull final DataLoaderService service,
+                                   @Nonnull final ChannelDescriptor descriptor,
+                                   @Nonnull final Throwable throwable )
+  {
+    _listeners.forEach( l -> l.onUnsubscribeFailed( service, descriptor, throwable ) );
+  }
+
+  @Override
+  public void onSubscriptionUpdateStarted( @Nonnull final DataLoaderService service,
+                                           @Nonnull final ChannelDescriptor descriptor )
+  {
+    _listeners.forEach( l -> l.onSubscriptionUpdateStarted( service, descriptor ) );
+  }
+
+  @Override
+  public void onSubscriptionUpdateCompleted( @Nonnull final DataLoaderService service,
+                                             @Nonnull final ChannelDescriptor descriptor )
+  {
+    _listeners.forEach( l -> l.onSubscriptionUpdateCompleted( service, descriptor ) );
+  }
+
+  @Override
+  public void onSubscriptionUpdateFailed( @Nonnull final DataLoaderService service,
+                                          @Nonnull final ChannelDescriptor descriptor,
+                                          @Nonnull final Throwable throwable )
+  {
+    _listeners.forEach( l -> l.onSubscriptionUpdateFailed( service, descriptor, throwable ) );
+  }
+}
---
+++ client-common/src/test/java/org/realityforge/replicant/client/transport/DataLoaderListenerSupportTest.java
@@ -0,0 +1,89 @@
+package org.realityforge.replicant.client.transport;
+
+import org.realityforge.replicant.client.ChannelDescriptor;
+import org.testng.annotations.Test;
+import static org.mockito.Mockito.*;
+import static org.testng.Assert.*;
+
+public class DataLoaderListenerSupportTest
+{
+  enum TestGraph
+  {
+    A
+  }
+
+  @SuppressWarnings( "ThrowableNotThrown" )
+  @Test
+  public void basicOperation()
+  {
+    final DataLoaderListenerSupport support = new DataLoaderListenerSupport();
+
+    final DataLoaderService service = mock( DataLoaderService.class );
+
+    final DataLoaderListener listener = mock( DataLoaderListener.class );
+    final Throwable throwable = new Throwable();
+    final ChannelDescriptor descriptor = new ChannelDescriptor( TestGraph.A );
+
+    assertTrue( support.addListener( listener ) );
+    assertFalse( support.addListener( listener ), "Can not add duplicate" );
+
+    reset( listener );
+    support.onConnect( service );
+    verify( listener ).onConnect( service );
+
+    reset( listener );
+    support.onInvalidConnect( service, throwable );
+    verify( listener ).onInvalidConnect( service, throwable );
+
+    reset( listener );
+    support.onDisconnect( service );
+    verify( listener ).onDisconnect( service );
+
+    reset( listener );
+    support.onInvalidDisconnect( service, throwable );
+    verify( listener ).onInvalidDisconnect( service, throwable );
+
+    reset( listener );
+    support.onSubscribeStarted( service, descriptor );
+    verify( listener ).onSubscribeStarted( service, descriptor );
+
+    reset( listener );
+    support.onSubscribeCompleted( service, descriptor );
+    verify( listener ).onSubscribeCompleted( service, descriptor );
+
+    reset( listener );
+    support.onSubscribeFailed( service, descriptor, throwable );
+    verify( listener ).onSubscribeFailed( service, descriptor, throwable );
+
+    reset( listener );
+    support.onSubscriptionUpdateStarted( service, descriptor );
+    verify( listener ).onSubscriptionUpdateStarted( service, descriptor );
+
+    reset( listener );
+    support.onSubscriptionUpdateCompleted( service, descriptor );
+    verify( listener ).onSubscriptionUpdateCompleted( service, descriptor );
+
+    reset( listener );
+    support.onSubscriptionUpdateFailed( service, descriptor, throwable );
+    verify( listener ).onSubscriptionUpdateFailed( service, descriptor, throwable );
+
+    reset( listener );
+    support.onUnsubscribeStarted( service, descriptor );
+    verify( listener ).onUnsubscribeStarted( service, descriptor );
+
+    reset( listener );
+    support.onUnsubscribeCompleted( service, descriptor );
+    verify( listener ).onUnsubscribeCompleted( service, descriptor );
+
+    reset( listener );
+    support.onUnsubscribeFailed( service, descriptor, throwable );
+    verify( listener ).onUnsubscribeFailed( service, descriptor, throwable );
+
+    assertTrue( support.removeListener( listener ) );
+    assertFalse( support.removeListener( listener ), "Can not remove duplicate" );
+
+    reset( listener );
+    support.onConnect( service );
+    verify( listener, never() ).onConnect( service );
+  }
+}
