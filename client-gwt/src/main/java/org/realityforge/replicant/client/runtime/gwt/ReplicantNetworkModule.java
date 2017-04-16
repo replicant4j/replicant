234bdb6073a8e1429f8018a96e22ea3cdf8762a2
Peter Donald <peter@realityforge.org>
2017-04-17 09:55:20 +1000

    Bund converger

--- client-gwt/src/main/java/org/realityforge/replicant/client/runtime/gwt/ReplicantNetworkModule.java
+++ client-gwt/src/main/java/org/realityforge/replicant/client/runtime/gwt/ReplicantNetworkModule.java
@@ -3,6 +3,7 @@ package org.realityforge.replicant.client.runtime.gwt;
 import com.google.gwt.inject.client.AbstractGinModule;
 import org.realityforge.replicant.client.runtime.AreaOfInterestService;
 import org.realityforge.replicant.client.runtime.AreaOfInterestServiceImpl;
+import org.realityforge.replicant.client.runtime.ContextConverger;
 import org.realityforge.replicant.client.runtime.ReplicantClientSystem;
 
 public class ReplicantNetworkModule
@@ -13,5 +14,6 @@ public class ReplicantNetworkModule
   {
     bind( AreaOfInterestService.class ).to( AreaOfInterestServiceImpl.class ).asEagerSingleton();
     bind( ReplicantClientSystem.class ).to( GwtReplicantClientSystemImpl.class ).asEagerSingleton();
+    bind( ContextConverger.class ).to( GwtContextConvergerImpl.class ).asEagerSingleton();
   }
 }
