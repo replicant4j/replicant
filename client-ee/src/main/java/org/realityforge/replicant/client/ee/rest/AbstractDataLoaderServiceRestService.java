b357ddebac4a7cf16ece272ee432e576a790d232
Peter Donald <peter@realityforge.org>
2017-04-20 16:52:01 +1000

    Add in base class for helping to monitor serverside replicant clients

---
+++ client-ee/src/main/java/org/realityforge/replicant/client/ee/rest/AbstractDataLoaderServiceRestService.java
@@ -0,0 +1,204 @@
+package org.realityforge.replicant.client.ee.rest;
+
+import java.io.StringWriter;
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import javax.annotation.Nonnull;
+import javax.annotation.PostConstruct;
+import javax.json.stream.JsonGenerator;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+import org.realityforge.replicant.client.ChannelDescriptor;
+import org.realityforge.replicant.client.ChannelSubscriptionEntry;
+import org.realityforge.replicant.client.EntitySubscriptionEntry;
+import org.realityforge.replicant.client.EntitySubscriptionManager;
+import org.realityforge.replicant.client.EntitySystem;
+import org.realityforge.replicant.client.transport.ClientSession;
+import org.realityforge.replicant.client.transport.DataLoaderService;
+import org.realityforge.replicant.shared.ee.JsonUtil;
+import org.realityforge.replicant.shared.ee.rest.AbstractReplicantRestService;
+
+/**
+ A base class from which to derive a replicant client rest service. A basic client will look like;
+
+<code>
+\@Path( MyDataLoaderServiceRestService.BASE_URL )
+\@Produces( MediaType.WILDCARD )
+\@ApplicationScoped
+\@Transactional( Transactional.TxType.NOT_SUPPORTED )
+public class MyDataLoaderServiceRestService
+  extends AbstractDataLoaderServiceRestService&lt;MyDataLoaderServiceRestService&gt;
+{
+  public static final String BASE_URL = CLIENT_URL_PREFIX + "my";
+
+  \@Inject
+  private MyEeDataLoaderService _dataLoaderService;
+  \@Inject
+  private EntitySystem _entitySystem;
+
+  \@Override
+  protected Class&lt;MyDataLoaderServiceRestService&gt; getCurrentResource()
+  {
+    return MyDataLoaderServiceRestService.class;
+  }
+
+  \@Override
+  \@Nonnull
+  protected EntitySystem getEntitySystem()
+  {
+    return _entitySystem;
+  }
+
+  \@Override
+  \@Nonnull
+  protected DataLoaderService getDataLoaderService()
+  {
+    return _dataLoaderService;
+  }
+}
+
+</code>
+ */
+public abstract class AbstractDataLoaderServiceRestService<T extends AbstractDataLoaderServiceRestService>
+  extends AbstractReplicantRestService
+{
+  public static final String CLIENT_URL_PREFIX = "replicant/client/";
+
+  protected abstract Class<T> getCurrentResource();
+
+  @Nonnull
+  protected abstract EntitySystem getEntitySystem();
+
+  @Nonnull
+  protected abstract DataLoaderService getDataLoaderService();
+
+  @PostConstruct
+  @Override
+  public void postConstruct()
+  {
+    super.postConstruct();
+  }
+
+  @Path( "connect" )
+  @GET
+  public Response connect( @Context final UriInfo uriInfo )
+  {
+    final DataLoaderService service = getDataLoaderService();
+    service.connect();
+    return redirectToStatus( uriInfo );
+  }
+
+  @Path( "disconnect" )
+  @GET
+  public Response disconnect( @Context final UriInfo uriInfo )
+  {
+    final DataLoaderService service = getDataLoaderService();
+    service.disconnect();
+    return redirectToStatus( uriInfo );
+  }
+
+  @GET
+  public Response getStatus()
+  {
+    return doGetStatus();
+  }
+
+  @Nonnull
+  protected Response redirectToStatus( final @Context UriInfo uriInfo )
+  {
+    final UriBuilder uriBuilder = uriInfo.getBaseUriBuilder();
+    uriBuilder.path( getCurrentResource() );
+    final URI resourceBaseUri = uriBuilder.build();
+    return Response.seeOther( resourceBaseUri ).build();
+  }
+
+  @Nonnull
+  protected Response doGetStatus()
+  {
+    final StringWriter writer = new StringWriter();
+    final JsonGenerator g = factory().createGenerator( writer );
+
+    final DataLoaderService service = getDataLoaderService();
+    g.writeStartObject();
+    g.write( "key", service.getKey() );
+    g.write( "state", service.getState().name() );
+    final ClientSession session = service.getSession();
+    if ( null != session )
+    {
+      g.writeStartObject( "session" );
+      g.write( "sessionID", session.getSessionID() );
+      g.write( "lastRxSequence", session.getLastRxSequence() );
+
+      emitChannels( g, service );
+      g.writeEnd();
+    }
+    g.writeEnd();
+
+    g.close();
+    return buildResponse( Response.ok(), writer.toString() );
+  }
+
+  private void emitChannels( final JsonGenerator g, final DataLoaderService service )
+  {
+    g.writeStartObject( "channels" );
+    final EntitySubscriptionManager subscriptionManager = getEntitySystem().getSubscriptionManager();
+    final Class<? extends Enum> graphType = service.getGraphType();
+    final List<Enum> typeSubscriptions =
+      subscriptionManager.getTypeSubscriptions().stream().
+        filter( e -> e.getClass() == graphType ).
+        collect( Collectors.toList() );
+    for ( final Enum e : typeSubscriptions )
+    {
+      emitSubscription( g, subscriptionManager.getSubscription( new ChannelDescriptor( e ) ) );
+    }
+    g.writeEnd();
+  }
+
+  private void emitSubscription( @Nonnull final JsonGenerator g, @Nonnull final ChannelSubscriptionEntry subscription )
+  {
+    final ChannelDescriptor descriptor = subscription.getDescriptor();
+    g.writeStartObject( descriptor.getGraph().name() );
+
+    emitID( g, descriptor );
+    final Object filter = subscription.getFilter();
+    if ( null != filter )
+    {
+      g.write( "filter", JsonUtil.toJsonObject( filter ) );
+    }
+    g.write( "explicitSubscription", subscription.isExplicitSubscription() );
+
+    emitEntities( g, subscription );
+
+    g.writeEnd();
+  }
+
+  private void emitEntities( @Nonnull final JsonGenerator g, @Nonnull final ChannelSubscriptionEntry subscription )
+  {
+    g.writeStartObject( "entities" );
+    final Map<Class<?>, Map<Object, EntitySubscriptionEntry>> entities = subscription.getEntities();
+    for ( final Map.Entry<Class<?>, Map<Object, EntitySubscriptionEntry>> entry : entities.entrySet() )
+    {
+      g.write( entry.getKey().getName(), entry.getValue().size() );
+    }
+    g.writeEnd();
+  }
+
+  private void emitID( final @Nonnull JsonGenerator g, final ChannelDescriptor descriptor )
+  {
+    final Object id = descriptor.getID();
+    if ( id instanceof String )
+    {
+      g.write( "id", (String) id );
+    }
+    else if ( id instanceof Integer )
+    {
+      g.write( "id", (Integer) id );
+    }
+  }
+}
