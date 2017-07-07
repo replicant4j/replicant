7cb2f3b966058eefd49acca8f3a696101cad255a
Peter Donald <peter@realityforge.org>
2017-07-07 15:26:35 +1000

    Fix spelling

--- client-common/src/main/java/org/realityforge/replicant/client/FilterUtil.java
+++ client-common/src/main/java/org/realityforge/replicant/client/FilterUtil.java
@@ -4,13 +4,13 @@ import java.util.Objects;
 import javax.annotation.Nullable;
 
 /**
- * Simple utility that is extracted so it can be replcaed by GWT compiler.
+ * Simple utility that is extracted so it can be replaced by GWT compiler.
  */
 public final class FilterUtil
 {
-  public static boolean filtersEqual( @Nullable final Object fitler1, @Nullable final Object fitler2 )
+  public static boolean filtersEqual( @Nullable final Object filter1, @Nullable final Object filter2 )
   {
-    return Objects.equals( fitler1, fitler2 );
+    return Objects.equals( filter1, filter2 );
   }
 
   private FilterUtil()
