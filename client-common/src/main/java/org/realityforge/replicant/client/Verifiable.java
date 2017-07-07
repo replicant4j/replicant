aee05774e817dc20b180800cf8d3e007233c6225
Peter Donald <peter@realityforge.org>
2017-07-07 15:25:29 +1000

    Fix spelling

--- client-common/src/main/java/org/realityforge/replicant/client/Verifiable.java
+++ client-common/src/main/java/org/realityforge/replicant/client/Verifiable.java
@@ -6,7 +6,7 @@ package org.realityforge.replicant.client;
 public interface Verifiable
 {
   /**
-   * Check the state of the entity and raise an exception if invalid oe any related entities are invalid..
+   * Check the state of the entity and raise an exception if invalid or any related entities are invalid.
    */
   void verify()
     throws Exception;
