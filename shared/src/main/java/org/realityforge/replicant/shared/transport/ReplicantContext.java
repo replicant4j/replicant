e247c110de32ae929700ed017e99316608bb49d6
Peter Donald <peter@realityforge.org>
2017-07-17 17:22:59 +1000

    Update header names to use the currect name

--- shared/src/main/java/org/realityforge/replicant/shared/transport/ReplicantContext.java
+++ shared/src/main/java/org/realityforge/replicant/shared/transport/ReplicantContext.java
@@ -26,15 +26,15 @@ public final class ReplicantContext
   /**
    * HTTP request header to indicate the session id.
    */
-  public static final String SESSION_ID_HEADER = "X-GWT-SessionID";
+  public static final String SESSION_ID_HEADER = "X-Replicant-SessionID";
   /**
    * HTTP request header to indicate the request id.
    */
-  public static final String REQUEST_ID_HEADER = "X-GWT-RequestID";
+  public static final String REQUEST_ID_HEADER = "X-Replicant-RequestID";
   /**
    * HTTP response header to indicate the whether the request is complete or a change set is expected.
    */
-  public static final String REQUEST_COMPLETE_HEADER = "X-GWT-RequestComplete";
+  public static final String REQUEST_COMPLETE_HEADER = "X-Replicant-RequestComplete";
   /**
    * The url relative to base from which replicant event stream data is retrieved.
    */
