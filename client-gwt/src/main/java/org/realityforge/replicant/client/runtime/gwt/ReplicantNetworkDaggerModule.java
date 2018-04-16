587217116acbe6897585c4567268548bc1d4d5c7
Peter Donald <peter@realityforge.org>
2018-04-16 10:42:49 +1000

    Expose service to dagger

--- client-gwt/src/main/java/org/realityforge/replicant/client/runtime/gwt/ReplicantNetworkDaggerModule.java
+++ client-gwt/src/main/java/org/realityforge/replicant/client/runtime/gwt/ReplicantNetworkDaggerModule.java
@@ -8,6 +8,8 @@ import org.realityforge.replicant.client.runtime.AreaOfInterestService;
 import org.realityforge.replicant.client.runtime.AreaOfInterestServiceImpl;
 import org.realityforge.replicant.client.runtime.ContextConverger;
 import org.realityforge.replicant.client.runtime.ReplicantClientSystem;
+import org.realityforge.replicant.client.runtime.ReplicantConnection;
+import org.realityforge.replicant.client.runtime.ReplicantConnectionImpl;
 
 @Module
 public class ReplicantNetworkDaggerModule
@@ -32,6 +34,14 @@ public class ReplicantNetworkDaggerModule
   @Provides
   @Singleton
   public static AreaOfInterestService provideAreaOfInterestService( @Nonnull final AreaOfInterestServiceImpl service )
+  {
+    return service;
+  }
+
+  @Nonnull
+  @Provides
+  @Singleton
+  public static ReplicantConnection provideReplicantConnection( @Nonnull final ReplicantConnectionImpl service )
   {
     return service;
   }
