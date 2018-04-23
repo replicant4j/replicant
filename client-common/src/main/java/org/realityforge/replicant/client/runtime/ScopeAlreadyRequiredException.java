cc11756b286ef6728b311cfef171ae0988fbbf35
Peter Donald <peter@realityforge.org>
2018-04-23 12:25:27 +1000

    Remove some unused exceptions

--- client-common/src/main/java/org/realityforge/replicant/client/runtime/ScopeAlreadyRequiredException.java
+++
@@ -1,7 +0,0 @@
-package org.realityforge.replicant.client.runtime;
-
-@SuppressWarnings( "GwtInconsistentSerializableClass" )
-public class ScopeAlreadyRequiredException
-  extends RuntimeException
-{
-}
