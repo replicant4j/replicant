104d191bca93f918ad336f231dc2f74b697bdf13
Peter Donald <peter@realityforge.org>
2018-05-03 11:15:12 +1000

    Add some javadocs

--- client/src/main/java/org/realityforge/replicant/client/aoi/AreaOfInterestService.java
+++ client/src/main/java/org/realityforge/replicant/client/aoi/AreaOfInterestService.java
@@ -12,34 +12,60 @@ import org.realityforge.replicant.client.ChannelAddress;
 import org.realityforge.replicant.client.FilterUtil;
 
 /**
- * The AreaOfInterestService is responsible for managing the expected subscriptions
- * that the client is interested in. The subscriptions may cross different replicant
- * systems, and may exist before the data sources have been connected. The AreaOfInterestService
- * intends to represent the desired state that the DataSources converge towards.
+ * The AreaOfInterestService is responsible for managing AreaOfInterest instance.
+ * An {@link AreaOfInterest} represents a delcaration of a desire for a
+ * {@link org.realityforge.replicant.client.subscription.Subscription}. The intention
+ * is that user code defines the desired state as instances of {@link AreaOfInterest}
+ * and the {@link org.realityforge.replicant.client.converger.ContextConverger} converges
+ * the actual state towards the desired state.
  */
 @Singleton
 @ArezComponent
 public abstract class AreaOfInterestService
   extends AbstractContainer<ChannelAddress, AreaOfInterest>
 {
+  /**
+   * Create an instance of the AreaOfInterestService.
+   *
+   * @return an instance of the AreaOfInterestService.
+   */
   @Nonnull
   public static AreaOfInterestService create()
   {
     return new Arez_AreaOfInterestService();
   }
 
+  /**
+   * Return the collection of AreaOfInterest that have been declared.
+   *
+   * @return the collection of AreaOfInterest that have been declared.
+   */
   @Nonnull
   public List<AreaOfInterest> getAreasOfInterest()
   {
     return RepositoryUtil.asList( entities() );
   }
 
+  /**
+   * Return a specific AreaOfInterest that has specified address.
+   *
+   * @param address the address of the channel that AreaOfInterest is about.
+   * @return the AreaOfInterest that matches if any.
+   */
   @Nullable
   public AreaOfInterest findAreaOfInterestByAddress( @Nonnull final ChannelAddress address )
   {
     return super.findByArezId( address );
   }
 
+  /**
+   * Locate an existing AreaOfInterest with specified address or create a new AreaOfInterest.
+   * The filter is updated, if required, to match the specified parameter.
+   *
+   * @param address the address of the channel that AreaOfInterest is about.
+   * @param filter  the filter that is used to define the channel.
+   * @return the AreaOfInterest.
+   */
   @Nonnull
   public AreaOfInterest findOrCreateAreaOfInterest( @Nonnull final ChannelAddress address,
                                                     @Nullable final Object filter )
