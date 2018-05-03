79a322c58596f4dfea72fae7b0fc3b243ed5f2ff
Peter Donald <peter@realityforge.org>
2018-05-03 22:59:17 +1000

    Copy the function shapes from Arez

---
+++ client/src/main/java/replicant/Procedure.java
@@ -0,0 +1,16 @@
+package replicant;
+
+/**
+ * Interface for performing an action that does not return a value.
+ */
+@FunctionalInterface
+public interface Procedure
+{
+  /**
+   * Perform an action, or throw an exception if unable to do so.
+   *
+   * @throws Throwable if unable to perform action.
+   */
+  void call()
+    throws Throwable;
+}
---
+++ client/src/main/java/replicant/SafeProcedure.java
@@ -0,0 +1,13 @@
+package replicant;
+
+/**
+ * Interface for performing an action that does not return a value.
+ */
+@FunctionalInterface
+public interface SafeProcedure
+{
+  /**
+   * Perform an action.
+   */
+  void call();
+}
