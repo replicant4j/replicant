a2ffa10122f62b72adc25774ff1af64ffe283668
Peter Donald <peter@realityforge.org>
2018-05-04 18:07:11 +1000

    Start a separate spy package

---
+++ client/src/main/java/replicant/spy/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Spy events and introspection utilities.
+ * The {@link replicant} package and this package are
+ * highly inter-dependent but they are kept separated as production builds
+ * typically compile out everything in this package when spies are disabled.
+ */
+@Unsupported( "This services is primary here to support DevTools and will evolve as requirements dictate" )
+package replicant.spy;
+
+import org.realityforge.anodoc.Unsupported;
