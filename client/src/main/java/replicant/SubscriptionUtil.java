38112b003ea2fdbf9f9f57f2bbbe45d6ed71f3b5
Peter Donald <peter@realityforge.org>
2018-05-04 16:38:34 +1000

    Extract out the old utils for cross data source subscription management

---
+++ client/src/main/java/replicant/SubscriptionUtil.java
@@ -0,0 +1,80 @@
+package replicant;
+
+import arez.Disposable;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+/**
+ * Utilities for integration across different datasources.
+ */
+public class SubscriptionUtil
+{
+  private SubscriptionUtil()
+  {
+  }
+
+  /**
+   * Convert object subscription to root object (specified object Type+ID) to target channels subscription
+   * based on function. If the root object is not yet present then return an empty stream.
+   * This is typically used by the function passed into the convergeCrossDataSourceSubscriptions() method.
+   */
+  @SuppressWarnings( "unchecked" )
+  @Nonnull
+  public static <T, O> Stream<O> instanceSubscriptionToValues( @Nonnull final Class<T> type,
+                                                               @Nonnull final Object id,
+                                                               @Nonnull final Function<T, Stream<O>> rootToStream )
+  {
+    final Entity entity = Replicant.context().findEntityByTypeAndId( type, id );
+    assert null != entity;
+    final T root = (T) entity.getUserObject();
+    return null != root ? rootToStream.apply( root ) : Stream.empty();
+  }
+
+  /**
+   * Converge subscriptions across data sources.
+   * All instances of the subscriptions to the source channelType within the scope are collected.
+   * The supplied function is used to generate a stream of expected subscriptions to the target channelType
+   * that are reachable from the source channelTypes. If an expected subscription is missing it is added,
+   * if an additional subscription is present then it is released.
+   */
+  public static void convergeCrossDataSourceSubscriptions( @Nonnull final Enum sourceChannelType,
+                                                           @Nonnull final Enum targetChannelType,
+                                                           @Nullable final Object filter,
+                                                           @Nonnull final Function<Object, Stream<Object>> sourceIDToTargetIDs )
+  {
+    // Need to check both subscription and filters are identical.
+    // If they are not the next step will either update the filters or add subscriptions
+    final ReplicantContext context = Replicant.context();
+    final Map<Object, Channel> existing =
+      context
+        .getAreasOfInterest()
+        .stream()
+        .filter( s -> s.getAddress().getChannelType().equals( targetChannelType ) )
+        .filter( subscription -> FilterUtil.filtersEqual( subscription.getChannel().getFilter(), filter ) )
+        .map( AreaOfInterest::getChannel )
+        .collect( Collectors.toMap( s -> s.getAddress().getId(), Function.identity() ) );
+
+    context
+      .getAreasOfInterest()
+      .stream()
+      .filter( s -> s.getAddress().getChannelType() == sourceChannelType )
+      .map( s -> s.getAddress().getId() )
+      .flatMap( sourceIDToTargetIDs )
+      .filter( Objects::nonNull )
+      .filter( id -> null == existing.remove( id ) )
+      .forEach( id -> context.findOrCreateAreaOfInterest( new ChannelAddress( targetChannelType, id ), filter ) );
+
+    context.getInstanceSubscriptionIds( sourceChannelType ).stream().
+      flatMap( sourceIDToTargetIDs ).
+      filter( Objects::nonNull ).
+      filter( id -> null == existing.remove( id ) ).
+      forEach( id -> context.findOrCreateAreaOfInterest( new ChannelAddress( targetChannelType, id ), filter ) );
+
+    existing.values().forEach( Disposable::dispose );
+  }
+}
---
+++ client/src/test/java/org/realityforge/replicant/client/runtime/SubscriptionUtilTest.java
@@ -0,0 +1,116 @@
+package org.realityforge.replicant.client.runtime;
+
+import replicant.AbstractReplicantTest;
+
+public class SubscriptionUtilTest
+  extends AbstractReplicantTest
+{
+/*
+  @Test
+  public void instanceSubscriptionToValues()
+  {
+    final TestRuntime r = new TestRuntime();
+
+    {
+      reset( r.getEntityLocator() );
+      when( r.getEntityLocator().findByID( String.class, 1 ) ).thenReturn( null );
+
+      final List<Object> list =
+        r.instanceSubscriptionToValues( String.class, 1, Stream::of ).
+          collect( Collectors.toList() );
+      assertEquals( list.size(), 0 );
+    }
+
+    {
+      final String entity2 = ValueUtil.randomString();
+
+      reset( r.getEntityLocator() );
+      when( r.getEntityLocator().findByID( String.class, 2 ) ).thenReturn( entity2 );
+
+      final List<Object> list =
+        r.instanceSubscriptionToValues( String.class, 2, Stream::of ).
+          collect( Collectors.toList() );
+      assertEquals( list.size(), 1 );
+      assertTrue( list.contains( entity2 ) );
+    }
+  }
+
+  @Test
+  public void doConvergeCrossDataSourceSubscriptions()
+  {
+    final String filter = ValueUtil.randomString();
+
+    final TestRuntime r = new TestRuntime();
+    final AreaOfInterestService aoiService = r.getAreaOfInterestService();
+    final ChannelAddress descriptor1 = new ChannelAddress( TestSystem.A, ValueUtil.nextID() );
+    final ChannelAddress descriptor2 = new ChannelAddress( TestSystem.A, ValueUtil.nextID() );
+    final ChannelAddress descriptor3 = new ChannelAddress( TestSystem.A, ValueUtil.nextID() );
+    final ChannelAddress descriptor4 = new ChannelAddress( TestSystem2.B, descriptor1.getId() );
+    final ChannelAddress descriptor5 = new ChannelAddress( TestSystem2.B, descriptor2.getId() );
+    final ChannelAddress descriptor6 = new ChannelAddress( TestSystem2.B, descriptor3.getId() );
+
+    // These descriptors have differing filters so Q should be updated
+    final ChannelAddress descriptorP = new ChannelAddress( TestSystem.A, ValueUtil.nextID() );
+    final ChannelAddress descriptorQ = new ChannelAddress( TestSystem2.B, descriptorP.getId() );
+
+    final Channel subscription1 = new Channel( aoiService, descriptor1 );
+    final Channel subscription2 = new Channel( aoiService, descriptor2 );
+    final Channel subscription4 = new Channel( aoiService, descriptor4 );
+    final Channel subscription5 = new Channel( aoiService, descriptor5 );
+    final Channel subscription6 = new Channel( aoiService, descriptor6 );
+
+    final Channel subscriptionP = new Channel( aoiService, descriptorP );
+    final Channel subscriptionQ = new Channel( aoiService, descriptorQ );
+
+    subscription1.setFilter( filter );
+    subscription2.setFilter( filter );
+    subscription4.setFilter( filter );
+    subscription5.setFilter( filter );
+    subscription6.setFilter( filter );
+
+    subscriptionP.setFilter( filter );
+    subscriptionQ.setFilter( ValueUtil.randomString() );
+
+    // Requires as yet uncreated subscription (subscription4)
+    final SubscriptionReference reference1 = scope.requireSubscription( subscription1 );
+
+    // Matches subscription5
+    final SubscriptionReference reference2 = scope.requireSubscription( subscription2 );
+
+    // next one should be retained
+    final SubscriptionReference reference5 = scope.requireSubscription( subscription5 );
+
+    // Next subscription should be released
+    scope.requireSubscription( subscription6 );
+
+    final SubscriptionReference referenceP = scope.requireSubscription( subscriptionP );
+    final SubscriptionReference referenceQ = scope.requireSubscription( subscriptionQ );
+
+    when( aoiService.findAreaOfInterestByAddress( descriptorQ ) ).thenReturn( subscriptionQ );
+    when( aoiService.createSubscription( descriptor4, filter ) ).thenReturn( subscription4 );
+    when( aoiService.createSubscriptionReference( descriptor1 ) ).thenReturn( reference1 );
+    when( aoiService.createSubscriptionReference( descriptor2 ) ).thenReturn( reference2 );
+    when( aoiService.createSubscriptionReference( descriptor4 ) ).thenReturn( subscription4.createReference() );
+    when( aoiService.createSubscriptionReference( descriptor5 ) ).thenReturn( reference5 );
+    when( aoiService.createSubscriptionReference( descriptorP ) ).thenReturn( referenceP );
+    when( aoiService.createSubscriptionReference( descriptorQ ) ).thenReturn( referenceQ );
+
+    r.doConvergeCrossDataSourceSubscriptions( scope, TestSystem.A, TestSystem2.B, filter, Stream::of );
+
+    assertEquals( scope.getRequiredSubscriptions().size(), 5 );
+    assertTrue( scope.getRequiredSubscriptions().contains( subscription5 ) );
+    assertTrue( scope.getRequiredSubscriptions().stream().anyMatch( s -> s.getAddress().equals( descriptor1 ) ) );
+    assertTrue( scope.getRequiredSubscriptions().stream().anyMatch( s -> s.getAddress().equals( descriptor2 ) ) );
+    assertTrue( scope.getRequiredSubscriptions().stream().anyMatch( s -> s.getAddress().equals( descriptor5 ) ) );
+    assertTrue( scope.getRequiredSubscriptions().stream().anyMatch( s -> s.getAddress().equals( descriptorP ) ) );
+    assertTrue( scope.getRequiredSubscriptions().stream().anyMatch( s -> s.getAddress().equals( descriptorQ ) ) );
+    assertFalse( scope.getRequiredSubscriptions().contains( subscription6 ) );
+
+    verify( aoiService, atLeast( 1 ) ).updateAreaOfInterest( subscriptionQ, filter );
+
+    //Will be called multiple times as we are dealing with mocks that do not callback to subscription to update state
+    verify( aoiService, atLeast( 1 ) ).destroySubscription( subscription6 );
+    verify( aoiService ).createSubscription( descriptor4, filter );
+  }
+  */
+}
