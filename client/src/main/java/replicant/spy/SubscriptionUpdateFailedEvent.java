1ba96cc58eafb6ba51be0447eb97f490216e48b5
Peter Donald <peter@realityforge.org>
2018-05-08 21:53:52 +1000

    Add SubscriptionUpdateFailedEvent spy event

---
+++ client/src/main/java/replicant/spy/SubscriptionUpdateFailedEvent.java
@@ -0,0 +1,60 @@
+package replicant.spy;
+
+import arez.spy.SerializableEvent;
+import java.util.Map;
+import java.util.Objects;
+import javax.annotation.Nonnull;
+import replicant.ChannelAddress;
+
+public final class SubscriptionUpdateFailedEvent
+  implements SerializableEvent
+{
+  @Nonnull
+  private final Class<?> _systemType;
+  @Nonnull
+  private final ChannelAddress _address;
+  @Nonnull
+  private final Throwable _error;
+
+  public SubscriptionUpdateFailedEvent( @Nonnull final Class<?> systemType,
+                                        @Nonnull final ChannelAddress address,
+                                        @Nonnull final Throwable error )
+  {
+    _systemType = Objects.requireNonNull( systemType );
+    _address = Objects.requireNonNull( address );
+    _error = Objects.requireNonNull( error );
+  }
+
+  @Nonnull
+  public Class<?> getSystemType()
+  {
+    return _systemType;
+  }
+
+  @Nonnull
+  public ChannelAddress getAddress()
+  {
+    return _address;
+  }
+
+  @Nonnull
+  public Throwable getError()
+  {
+    return _error;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void toMap( @Nonnull final Map<String, Object> map )
+  {
+    map.put( "type", "DataLoader.SubscriptionUpdateFailed" );
+    map.put( "systemType", getSystemType().getSimpleName() );
+    final ChannelAddress address = getAddress();
+    map.put( "channel.type", address.getChannelType().name() );
+    map.put( "channel.id", address.getId() );
+    final Throwable throwable = getError();
+    map.put( "message", null == throwable.getMessage() ? throwable.toString() : throwable.getMessage() );
+  }
+}
---
+++ client/src/test/java/replicant/spy/SubscriptionUpdateFailedEventTest.java
@@ -0,0 +1,38 @@
+package replicant.spy;
+
+import arez.Arez;
+import java.util.HashMap;
+import org.testng.annotations.Test;
+import replicant.AbstractReplicantTest;
+import replicant.ChannelAddress;
+import static org.testng.Assert.*;
+
+public class SubscriptionUpdateFailedEventTest
+  extends AbstractReplicantTest
+{
+  @Test
+  public void basicOperation()
+  {
+    final ChannelAddress address = new ChannelAddress( G.G1 );
+    final SubscriptionUpdateFailedEvent event =
+      new SubscriptionUpdateFailedEvent( G.class, address, new Error( "Something Bad Happened" ) );
+
+    assertEquals( event.getSystemType(), G.class );
+    assertEquals( event.getAddress(), address );
+
+    final HashMap<String, Object> data = new HashMap<>();
+    Arez.context().safeAction( () -> event.toMap( data ) );
+
+    assertEquals( data.get( "type" ), "DataLoader.SubscriptionUpdateFailed" );
+    assertEquals( data.get( "systemType" ), "G" );
+    assertEquals( data.get( "channel.type" ), "G1" );
+    assertEquals( data.get( "channel.id" ), address.getId() );
+    assertEquals( data.get( "message" ), "Something Bad Happened" );
+    assertEquals( data.size(), 5 );
+  }
+
+  enum G
+  {
+    G1
+  }
+}
