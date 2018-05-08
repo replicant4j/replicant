a0dff7c6865967ad01bd05159ec1a00328fe9144
Peter Donald <peter@realityforge.org>
2018-05-09 08:54:47 +1000

    Remove GwtReplicantClientSystem as can be replaced by @Autorun

--- client/src/main/java/org/realityforge/replicant/client/gwt/ReplicantDaggerModule.java
+++ client/src/main/java/org/realityforge/replicant/client/gwt/ReplicantDaggerModule.java
@@ -6,23 +6,14 @@ import javax.annotation.Nonnull;
 import javax.inject.Singleton;
 import org.realityforge.replicant.client.transport.CacheService;
 import org.realityforge.replicant.client.transport.ConvergerDaggerModule;
-import org.realityforge.replicant.client.transport.ReplicantClientSystem;
 
-@Module( includes = { GwtReplicantClientSystemDaggerModule.class, ConvergerDaggerModule.class } )
+@Module( includes = { ConvergerDaggerModule.class } )
 public class ReplicantDaggerModule
 {
   @Nonnull
   @Provides
   @Singleton
   public static CacheService provideCacheService( @Nonnull final LocalCacheService service )
-  {
-    return service;
-  }
-
-  @Nonnull
-  @Provides
-  @Singleton
-  static ReplicantClientSystem provideReplicantClientSystem( @Nonnull final GwtReplicantClientSystem service )
   {
     return service;
   }
