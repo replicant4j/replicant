c0922a49f730dbfb84c3e6ee270db882696ee29d
Peter Donald <peter@realityforge.org>
2018-05-08 21:53:16 +1000

    Add UnsubscribeCompletedEvent spy event

---
+++ client/src/main/java/replicant/spy/UnsubscribeCompletedEvent.java
@@ -0,0 +1,47 @@
+package replicant.spy;
+
+import arez.spy.SerializableEvent;
+import java.util.Map;
+import java.util.Objects;
+import javax.annotation.Nonnull;
+import replicant.ChannelAddress;
+
+public final class UnsubscribeCompletedEvent
+  implements SerializableEvent
+{
+  @Nonnull
+  private final Class<?> _systemType;
+  @Nonnull
+  private final ChannelAddress _address;
+
+  public UnsubscribeCompletedEvent( @Nonnull final Class<?> systemType, @Nonnull final ChannelAddress address )
+  {
+    _systemType = Objects.requireNonNull( systemType );
+    _address = Objects.requireNonNull( address );
+  }
+
+  @Nonnull
+  public Class<?> getSystemType()
+  {
+    return _systemType;
+  }
+
+  @Nonnull
+  public ChannelAddress getAddress()
+  {
+    return _address;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void toMap( @Nonnull final Map<String, Object> map )
+  {
+    map.put( "type", "DataLoader.UnsubscribeCompleted" );
+    map.put( "systemType", getSystemType().getSimpleName() );
+    final ChannelAddress address = getAddress();
+    map.put( "channel.type", address.getChannelType().name() );
+    map.put( "channel.id", address.getId() );
+  }
+}
