dbcb6c1e4921b118cf79298b72bf82eea263dd01
Peter Donald <peter@realityforge.org>
2018-05-08 22:43:31 +1000

    Merge ReplicantNetworkDaggerModule into ReplicantDaggerModule

--- client/src/main/java/org/realityforge/replicant/client/gwt/ReplicantDaggerModule.java
+++ client/src/main/java/org/realityforge/replicant/client/gwt/ReplicantDaggerModule.java
@@ -5,14 +5,24 @@ import dagger.Provides;
 import javax.annotation.Nonnull;
 import javax.inject.Singleton;
 import org.realityforge.replicant.client.transport.CacheService;
+import org.realityforge.replicant.client.transport.ConvergerDaggerModule;
+import org.realityforge.replicant.client.transport.ReplicantClientSystem;
 
-@Module
+@Module( includes = { GwtReplicantClientSystemDaggerModule.class, ConvergerDaggerModule.class } )
 public class ReplicantDaggerModule
 {
   @Nonnull
   @Provides
   @Singleton
   public static CacheService provideCacheService( @Nonnull final LocalCacheService service )
+  {
+    return service;
+  }
+
+  @Nonnull
+  @Provides
+  @Singleton
+  static ReplicantClientSystem provideReplicantClientSystem( @Nonnull final GwtReplicantClientSystem service )
   {
     return service;
   }
