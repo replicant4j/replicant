968ab9dc48813966e20b1cf3d214198e2d3a4f07
Peter Donald <peter@realityforge.org>
2018-05-08 21:52:02 +1000

    Add SubscribeFailedEvent spy event

---
+++ client/src/main/java/replicant/spy/SubscribeFailedEvent.java
@@ -0,0 +1,63 @@
+package replicant.spy;
+
+import arez.spy.SerializableEvent;
+import java.util.Map;
+import java.util.Objects;
+import javax.annotation.Nonnull;
+import replicant.ChannelAddress;
+
+/**
+ * Notification when an DataLoader fails to subscribe to a channel.
+ */
+public final class SubscribeFailedEvent
+  implements SerializableEvent
+{
+  @Nonnull
+  private final Class<?> _systemType;
+  @Nonnull
+  private final ChannelAddress _address;
+  @Nonnull
+  private final Throwable _error;
+
+  public SubscribeFailedEvent( @Nonnull final Class<?> systemType,
+                               @Nonnull final ChannelAddress address,
+                               @Nonnull final Throwable error )
+  {
+    _systemType = Objects.requireNonNull( systemType );
+    _address = Objects.requireNonNull( address );
+    _error = Objects.requireNonNull( error );
+  }
+
+  @Nonnull
+  public Class<?> getSystemType()
+  {
+    return _systemType;
+  }
+
+  @Nonnull
+  public ChannelAddress getAddress()
+  {
+    return _address;
+  }
+
+  @Nonnull
+  public Throwable getError()
+  {
+    return _error;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void toMap( @Nonnull final Map<String, Object> map )
+  {
+    map.put( "type", "DataLoader.SubscribeFailed" );
+    map.put( "systemType", getSystemType().getSimpleName() );
+    final ChannelAddress address = getAddress();
+    map.put( "channel.type", address.getChannelType().name() );
+    map.put( "channel.id", address.getId() );
+    final Throwable throwable = getError();
+    map.put( "message", null == throwable.getMessage() ? throwable.toString() : throwable.getMessage() );
+  }
+}
