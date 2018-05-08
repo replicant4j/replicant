0dac2a9bde2b2d884dff9a479f2536d969de5abb
Peter Donald <peter@realityforge.org>
2018-05-08 15:12:33 +1000

    Move class inline to test that uses it

--- client/src/test/java/org/realityforge/replicant/client/transport/ReplicantClientSystemTest.java
+++ client/src/test/java/org/realityforge/replicant/client/transport/ReplicantClientSystemTest.java
@@ -1,8 +1,10 @@
 package org.realityforge.replicant.client.transport;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import org.testng.annotations.Test;
 import replicant.AbstractReplicantTest;
+import replicant.ChannelAddress;
 import static org.testng.Assert.*;
 
 public class ReplicantClientSystemTest
@@ -460,5 +462,104 @@ public class ReplicantClientSystemTest
   private TestDataLoaderService newServiceC()
   {
     return new TestDataLoaderService( TestSystemC.class );
+  }
+
+  static final class TestDataLoaderService
+    implements DataLoaderService
+  {
+    private final Class<? extends Enum> _systemType;
+    private State _state;
+    private boolean _connectCalled;
+    private boolean _disconnectCalled;
+
+    TestDataLoaderService( @Nonnull final Class<? extends Enum> systemType )
+    {
+      _systemType = systemType;
+      _state = State.DISCONNECTED;
+    }
+
+    void setState( @Nonnull final State state )
+    {
+      _state = state;
+    }
+
+    @Nonnull
+    @Override
+    public State getState()
+    {
+      return _state;
+    }
+
+    void reset()
+    {
+      _connectCalled = false;
+      _disconnectCalled = false;
+    }
+
+    @Override
+    public void connect()
+    {
+      _connectCalled = true;
+    }
+
+    boolean isConnectCalled()
+    {
+      return _connectCalled;
+    }
+
+    @Override
+    public void disconnect()
+    {
+      _disconnectCalled = true;
+    }
+
+    boolean isDisconnectCalled()
+    {
+      return _disconnectCalled;
+    }
+
+    @Override
+    public void scheduleDataLoad()
+    {
+    }
+
+    @Nonnull
+    @Override
+    public Class<? extends Enum> getSystemType()
+    {
+      return _systemType;
+    }
+
+    @Override
+    public boolean isAreaOfInterestActionPending( @Nonnull final AreaOfInterestAction action,
+                                                  @Nonnull final ChannelAddress address,
+                                                  @Nullable final Object filter )
+    {
+      return false;
+    }
+
+    @Override
+    public void requestSubscribe( @Nonnull final ChannelAddress address, @Nullable final Object filterParameter )
+    {
+    }
+
+    @Override
+    public void requestSubscriptionUpdate( @Nonnull final ChannelAddress address,
+                                           @Nullable final Object filterParameter )
+    {
+    }
+
+    @Override
+    public void requestUnsubscribe( @Nonnull final ChannelAddress address )
+    {
+    }
+
+    @Override
+    public int indexOfPendingAreaOfInterestAction( @Nonnull final AreaOfInterestAction action,
+                                                   @Nonnull final ChannelAddress address,
+                                                   @Nullable final Object filter )
+    {
+      return -1;
+    }
   }
 }
