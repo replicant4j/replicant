e0d148626ede1dd82b005559272d03b9085a0cec
Peter Donald <peter@realityforge.org>
2018-05-08 10:35:13 +1000

    Rename parameters as appropriate

--- client/src/main/java/org/realityforge/replicant/client/transport/DataLoaderListener.java
+++ client/src/main/java/org/realityforge/replicant/client/transport/DataLoaderListener.java
@@ -13,7 +13,7 @@ public interface DataLoaderListener
   /**
    * Invoked to fire an event when failed to connect.
    */
-  void onInvalidDisconnect( @Nonnull DataLoaderService service, @Nonnull Throwable throwable );
+  void onInvalidDisconnect( @Nonnull DataLoaderService service, @Nonnull Throwable error );
 
   /**
    * Invoked to fire an event when disconnect has completed.
@@ -23,7 +23,7 @@ public interface DataLoaderListener
   /**
    * Invoked to fire an event when failed to connect.
    */
-  void onInvalidConnect( @Nonnull DataLoaderService service, @Nonnull Throwable throwable );
+  void onInvalidConnect( @Nonnull DataLoaderService service, @Nonnull Throwable error );
 
   /**
    * Called when a data load has completed.
@@ -33,34 +33,34 @@ public interface DataLoaderListener
   /**
    * Called when a data load has resulted in a failure.
    */
-  void onDataLoadFailure( @Nonnull DataLoaderService service, @Nonnull Throwable throwable );
+  void onDataLoadFailure( @Nonnull DataLoaderService service, @Nonnull Throwable error );
 
   /**
    * Attempted to retrieve data from backend and failed.
    */
-  void onPollFailure( @Nonnull DataLoaderService service, @Nonnull Throwable throwable );
+  void onPollFailure( @Nonnull DataLoaderService service, @Nonnull Throwable error );
 
-  void onSubscribeStarted( @Nonnull DataLoaderService service, @Nonnull ChannelAddress descriptor );
+  void onSubscribeStarted( @Nonnull DataLoaderService service, @Nonnull ChannelAddress address );
 
-  void onSubscribeCompleted( @Nonnull DataLoaderService service, @Nonnull ChannelAddress descriptor );
+  void onSubscribeCompleted( @Nonnull DataLoaderService service, @Nonnull ChannelAddress address );
 
   void onSubscribeFailed( @Nonnull DataLoaderService service,
-                          @Nonnull ChannelAddress descriptor,
-                          @Nonnull Throwable throwable );
+                          @Nonnull ChannelAddress address,
+                          @Nonnull Throwable error );
 
-  void onUnsubscribeStarted( @Nonnull DataLoaderService service, @Nonnull ChannelAddress descriptor );
+  void onUnsubscribeStarted( @Nonnull DataLoaderService service, @Nonnull ChannelAddress address );
 
-  void onUnsubscribeCompleted( @Nonnull DataLoaderService service, @Nonnull ChannelAddress descriptor );
+  void onUnsubscribeCompleted( @Nonnull DataLoaderService service, @Nonnull ChannelAddress address );
 
   void onUnsubscribeFailed( @Nonnull DataLoaderService service,
-                            @Nonnull ChannelAddress descriptor,
-                            @Nonnull Throwable throwable );
+                            @Nonnull ChannelAddress address,
+                            @Nonnull Throwable error );
 
-  void onSubscriptionUpdateStarted( @Nonnull DataLoaderService service, @Nonnull ChannelAddress descriptor );
+  void onSubscriptionUpdateStarted( @Nonnull DataLoaderService service, @Nonnull ChannelAddress address );
 
-  void onSubscriptionUpdateCompleted( @Nonnull DataLoaderService service, @Nonnull ChannelAddress descriptor );
+  void onSubscriptionUpdateCompleted( @Nonnull DataLoaderService service, @Nonnull ChannelAddress address );
 
   void onSubscriptionUpdateFailed( @Nonnull DataLoaderService service,
-                                   @Nonnull ChannelAddress descriptor,
-                                   @Nonnull Throwable throwable );
+                                   @Nonnull ChannelAddress address,
+                                   @Nonnull Throwable error );
 }
