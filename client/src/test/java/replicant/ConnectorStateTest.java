a3592fb4aec1be45b2766d11841039fee89e68a4
Peter Donald <peter@realityforge.org>
2018-05-09 18:10:08 +1000

    Add some tests around isTransitionState

---
+++ client/src/test/java/replicant/ConnectorStateTest.java
@@ -0,0 +1,19 @@
+package replicant;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class ConnectorStateTest
+  extends AbstractReplicantTest
+{
+  @Test
+  public void isTransitionState()
+    throws Exception
+  {
+    assertEquals( ConnectorState.isTransitionState( ConnectorState.CONNECTING ), false );
+    assertEquals( ConnectorState.isTransitionState( ConnectorState.CONNECTED ),false );
+    assertEquals( ConnectorState.isTransitionState( ConnectorState.DISCONNECTING ),true );
+    assertEquals( ConnectorState.isTransitionState( ConnectorState.DISCONNECTED ),false );
+    assertEquals( ConnectorState.isTransitionState( ConnectorState.ERROR ),false );
+  }
+}
