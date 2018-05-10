5c2aa1a0a7b3211fe60a53e16f14dd81037347d9
Peter Donald <peter@realityforge.org>
2018-05-10 15:31:33 +1000

    Convert comments into javadoc comments

--- client/src/main/java/replicant/ConnectorState.java
+++ client/src/main/java/replicant/ConnectorState.java
@@ -7,15 +7,25 @@ import javax.annotation.Nonnull;
  */
 public enum ConnectorState
 {
-  /// The service is not yet connected or has been disconnected
+  /**
+   * The service is not yet connected or has been disconnected
+   */
   DISCONNECTED,
-  /// The service has started connecting but connection has not completed.
+  /**
+   * The service has started connecting but connection has not completed.
+   */
   CONNECTING,
-  /// The service is connected.
+  /**
+   * The service is connected.
+   */
   CONNECTED,
-  /// The service has started disconnecting but disconnection has not completed.
+  /**
+   * The service has started disconnecting but disconnection has not completed.
+   */
   DISCONNECTING,
-  /// The service is in error state. This error may occur during connection, disconnection or in normal operation.
+  /**
+   * The service is in error state. This error may occur during connection, disconnection or in normal operation.
+   */
   ERROR;
 
   /**
