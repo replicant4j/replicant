e6a33af1d5dcfd763b1cac36fe4c162db5c64c39
Peter Donald <peter@realityforge.org>
2018-05-15 15:07:23 +1000

    Add test CacheService

---
+++ client/src/test/java/replicant/TestCacheService.java
@@ -0,0 +1,54 @@
+package replicant;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import replicant.spi.CacheEntry;
+import replicant.spi.CacheService;
+
+public class TestCacheService
+  implements CacheService
+{
+  private final Map<String, String> _data = new HashMap<>();
+
+  @Nullable
+  @Override
+  public CacheEntry lookup( @Nonnull final String key )
+  {
+    final String eTag = _data.get( key + "ETAG" );
+    final String content = _data.get( key );
+    if ( null != eTag && null != content )
+    {
+      return new CacheEntry( key, eTag, content );
+    }
+    else
+    {
+      return null;
+    }
+  }
+
+  @Override
+  public boolean store( @Nonnull final String key, @Nonnull final String eTag, @Nonnull final String content )
+  {
+    _data.put( key + "ETAG", eTag );
+    _data.put( key, content );
+    return true;
+
+  }
+
+  @Override
+  public boolean invalidate( @Nonnull final String key )
+  {
+    if ( !_data.containsKey( key + "ETAG" ) )
+    {
+      return false;
+    }
+    else
+    {
+      _data.remove( key + "ETAG" );
+      _data.remove( key );
+      return true;
+    }
+  }
+}
