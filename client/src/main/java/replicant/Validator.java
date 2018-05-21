0b54092a8ff9b79d6604d841e2c38b28ee8effb8
Peter Donald <peter@realityforge.org>
2018-05-21 15:47:29 +1000

    Add Validator and associated test

---
+++ client/src/main/java/replicant/Validator.java
@@ -0,0 +1,54 @@
+package replicant;
+
+import arez.annotations.Action;
+import arez.annotations.ArezComponent;
+import javax.annotation.Nullable;
+import static org.realityforge.braincheck.Guards.*;
+
+/**
+ * A utility class that is used to validate state within the replicant context is consistent.
+ */
+@ArezComponent
+abstract class Validator
+  extends ReplicantService
+{
+  static Validator create( @Nullable final ReplicantContext context )
+  {
+    return new Arez_Validator( context );
+  }
+
+  Validator( @Nullable final ReplicantContext context )
+  {
+    super( context );
+  }
+
+  /**
+   * Verify that all entities contained within the EntityService will pass verification.
+   * An entity can be verified by implementing the {@link Verifiable} interface.
+   */
+  @Action
+  void validateEntities()
+  {
+    if ( Replicant.shouldCheckInvariants() )
+    {
+      for ( final Class<?> entityType : getReplicantContext().findAllEntityTypes() )
+      {
+        for ( final Entity entity : getReplicantContext().findAllEntitiesByType( entityType ) )
+        {
+          try
+          {
+            final Object userObject = entity.getUserObject();
+            if ( null != userObject )
+            {
+              Verifiable.verify( userObject );
+            }
+          }
+          catch ( final Exception e )
+          {
+            fail( () -> "Replicant-0065: Entity failed to verify during validation process. Entity = " + entity );
+          }
+        }
+      }
+    }
+  }
+}
---
+++ client/src/test/java/replicant/ValidatorTest.java
@@ -0,0 +1,64 @@
+package replicant;
+
+import javax.annotation.Nullable;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class ValidatorTest
+  extends AbstractReplicantTest
+{
+  @Test
+  public void noEntities()
+  {
+    Validator.create( null ).validateEntities();
+  }
+
+  @Test
+  public void entitiesAllValid()
+  {
+    final EntityService entityService = Replicant.context().getEntityService();
+    final Entity entity1 = safeAction( () -> entityService.findOrCreateEntity( "MyEntity/1", MyEntity.class, 1 ) );
+    safeAction( () -> entity1.setUserObject( new MyEntity( null ) ) );
+    final Entity entity2 = safeAction( () -> entityService.findOrCreateEntity( "MyEntity/2", MyEntity.class, 2 ) );
+
+    // Entities fine
+    Validator.create( null ).validateEntities();
+  }
+
+  @Test
+  public void invalidEntity()
+  {
+    final EntityService entityService = Replicant.context().getEntityService();
+    final Entity entity1 = safeAction( () -> entityService.findOrCreateEntity( "MyEntity/1", MyEntity.class, 1 ) );
+    final Exception error = new Exception();
+    safeAction( () -> entity1.setUserObject( new MyEntity( error ) ) );
+
+    final IllegalStateException exception =
+      expectThrows( IllegalStateException.class, () -> Validator.create( null ).validateEntities() );
+
+    assertEquals( exception.getMessage(),
+                  "Replicant-0065: Entity failed to verify during validation process. Entity = MyEntity/1" );
+  }
+
+  static class MyEntity
+    implements Verifiable
+  {
+    @Nullable
+    private final Exception _exception;
+
+    MyEntity( @Nullable final Exception exception )
+    {
+      _exception = exception;
+    }
+
+    @Override
+    public void verify()
+      throws Exception
+    {
+      if ( null != _exception )
+      {
+        throw _exception;
+      }
+    }
+  }
+}
