d05dc1cb20134c1c67c35a1f9769512c6c16c4dd
Peter Donald <peter@realityforge.org>
2018-05-25 15:59:40 +1000

    Expose hasCompleted

--- client/src/main/java/replicant/Request.java
+++ client/src/main/java/replicant/Request.java
@@ -33,7 +33,7 @@ public final class Request
   {
     if ( Replicant.shouldCheckApiInvariants() )
     {
-      apiInvariant( () -> !_entry.hasCompleted(),
+      apiInvariant( () -> !this.hasCompleted(),
                     () -> "Replicant-0073: Request.onSuccess invoked on completed request " + _entry + "." );
     }
     _entry.setNormalCompletion( true );
@@ -45,12 +45,17 @@ public final class Request
   {
     if ( Replicant.shouldCheckApiInvariants() )
     {
-      apiInvariant( () -> !_entry.hasCompleted(),
+      apiInvariant( () -> !this.hasCompleted(),
                     () -> "Replicant-0074: Request.onFailure invoked on completed request " + _entry + "." );
     }
     _entry.setNormalCompletion( false );
     _entry.setExpectingResults( false );
     _connection.completeRequest( _entry, Objects.requireNonNull( onError ) );
+  }
+
+  public boolean hasCompleted()
+  {
+    return _entry.hasCompleted();
   }
 
   @TestOnly
