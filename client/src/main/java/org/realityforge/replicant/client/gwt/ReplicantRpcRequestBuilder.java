70fb2cd3f32523da29e6aa6ff5b8e604c87a77e7
Peter Donald <peter@realityforge.org>
2018-05-25 16:13:06 +1000

    Rewrite ReplicantRpcRequestBuilder

--- client/src/main/java/org/realityforge/replicant/client/gwt/ReplicantRpcRequestBuilder.java
+++ client/src/main/java/org/realityforge/replicant/client/gwt/ReplicantRpcRequestBuilder.java
@@ -1,66 +1,23 @@
 package org.realityforge.replicant.client.gwt;
 
-import com.google.gwt.http.client.Request;
 import com.google.gwt.http.client.RequestBuilder;
 import com.google.gwt.http.client.RequestCallback;
-import com.google.gwt.http.client.Response;
 import com.google.gwt.user.client.rpc.RpcRequestBuilder;
 import javax.annotation.Nonnull;
-import org.realityforge.replicant.client.transport.SessionContext;
 import org.realityforge.replicant.shared.SharedConstants;
-import replicant.Connection;
-import replicant.RequestEntry;
 
-public class ReplicantRpcRequestBuilder
+public abstract class ReplicantRpcRequestBuilder
   extends RpcRequestBuilder
 {
   @Nonnull
-  private final SessionContext _sessionContext;
-
-  public ReplicantRpcRequestBuilder( @Nonnull final SessionContext sessionContext )
-  {
-    _sessionContext = sessionContext;
-  }
+  protected abstract replicant.Request getRequest();
 
   @Override
   protected void doSetCallback( final RequestBuilder rb, final RequestCallback callback )
   {
-    final Connection connection = _sessionContext.getConnection();
-    if ( null != connection )
-    {
-      rb.setHeader( SharedConstants.CONNECTION_ID_HEADER, connection.getConnectionId() );
-    }
-    final RequestEntry entry = _sessionContext.getRequest();
-    if ( null == entry )
-    {
-      rb.setCallback( callback );
-    }
-    else
-    {
-      rb.setHeader( SharedConstants.REQUEST_ID_HEADER, String.valueOf( entry.getRequestId() ) );
-      rb.setCallback( new RequestCallback()
-      {
-        @Override
-        public void onResponseReceived( final Request request, final Response response )
-        {
-          final boolean messageComplete = "1".equals( response.getHeader( SharedConstants.REQUEST_COMPLETE_HEADER ) );
-          entry.setExpectingResults( !messageComplete );
-          if ( null != callback )
-          {
-            callback.onResponseReceived( request, response );
-          }
-        }
-
-        @Override
-        public void onError( final Request request, final Throwable exception )
-        {
-          entry.setExpectingResults( false );
-          if ( null != callback )
-          {
-            callback.onError( request, exception );
-          }
-        }
-      } );
-    }
+    final replicant.Request r = getRequest();
+    rb.setHeader( SharedConstants.CONNECTION_ID_HEADER, r.getConnectionId() );
+    rb.setHeader( SharedConstants.REQUEST_ID_HEADER, String.valueOf( r.getRequestId() ) );
+    rb.setCallback( new ReplicantRequestCallback( r, callback ) );
   }
 }
--- client/src/test/java/org/realityforge/replicant/client/gwt/ReplicantRpcRequestBuilderTest.java
+++ client/src/test/java/org/realityforge/replicant/client/gwt/ReplicantRpcRequestBuilderTest.java
@@ -1,138 +1,60 @@
 package org.realityforge.replicant.client.gwt;
 
-import com.google.gwt.http.client.Request;
 import com.google.gwt.http.client.RequestBuilder;
 import com.google.gwt.http.client.RequestCallback;
-import com.google.gwt.http.client.Response;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
+import javax.annotation.Nonnull;
 import org.realityforge.guiceyloops.shared.ValueUtil;
-import org.realityforge.replicant.client.transport.SessionContext;
 import org.realityforge.replicant.shared.SharedConstants;
 import org.testng.annotations.Test;
 import replicant.AbstractReplicantTest;
-import replicant.Connection;
-import replicant.RequestEntry;
+import replicant.Replicant;
 import replicant.TestConnector;
 import static org.mockito.Mockito.*;
-import static org.testng.Assert.*;
 
 public class ReplicantRpcRequestBuilderTest
   extends AbstractReplicantTest
 {
   @Test
-  public void noContext()
+  public void onSuccess()
   {
+    final TestConnector connector = TestConnector.create( newSchema() );
+    newConnection( connector );
+    final replicant.Request r =
+      Replicant.context().newRequest( connector.getSchema().getId(), ValueUtil.randomString() );
+
     final RequestBuilder rb = mock( RequestBuilder.class );
-    final RequestCallback callback = mock( RequestCallback.class );
-    final SessionContext sessionContext = new SessionContext();
-    new ReplicantRpcRequestBuilder( sessionContext ).doSetCallback( rb, callback );
-    verify( rb ).setCallback( callback );
-    verify( rb, never() ).setHeader( refEq( SharedConstants.CONNECTION_ID_HEADER ), anyString() );
-    verify( rb, never() ).setHeader( refEq( SharedConstants.REQUEST_ID_HEADER ), anyString() );
+    final RequestCallback chainedCallback = mock( RequestCallback.class );
+
+    final AtomicReference<RequestCallback> callback = new AtomicReference<>();
+    doAnswer( i -> {
+      callback.set( (RequestCallback) i.getArguments()[ 0 ] );
+      return null;
+    } ).when( rb ).setCallback( any( RequestCallback.class ) );
+
+    new TestReplicantRpcRequestBuilder( r ).doSetCallback( rb, chainedCallback );
+
+    verify( rb ).setCallback( any( ReplicantRequestCallback.class ) );
+    verify( rb ).setHeader( eq( SharedConstants.CONNECTION_ID_HEADER ), eq( r.getConnectionId() ) );
+    verify( rb ).setHeader( eq( SharedConstants.REQUEST_ID_HEADER ), eq( String.valueOf( r.getRequestId() ) ) );
   }
 
-  @Test
-  public void connectionIdSet()
+  static class TestReplicantRpcRequestBuilder
+    extends ReplicantRpcRequestBuilder
   {
-    final RequestBuilder rb = mock( RequestBuilder.class );
-    final RequestCallback callback = mock( RequestCallback.class );
-    final SessionContext sessionContext = new SessionContext();
-    sessionContext.setConnection( new Connection( TestConnector.create(), "1" ) );
-    new ReplicantRpcRequestBuilder( sessionContext ).doSetCallback( rb, callback );
-    verify( rb ).setCallback( callback );
-    verify( rb ).setHeader( refEq( SharedConstants.CONNECTION_ID_HEADER ), refEq( "1" ) );
-    verify( rb, never() ).setHeader( refEq( SharedConstants.REQUEST_ID_HEADER ), anyString() );
-  }
+    private final replicant.Request _request;
 
-  @Test
-  public void requestIDSet_withSuccessAndComplete()
-    throws Exception
-  {
-    final Connection connection = new Connection( TestConnector.create(), ValueUtil.randomString() );
-    final RequestEntry requestEntry = connection.newRequest( "", null );
-    final SessionContext sessionContext = new SessionContext();
-    sessionContext.setConnection( connection );
-    setRequest( sessionContext, requestEntry );
-    final RequestBuilder rb = mock( RequestBuilder.class );
-    final RequestCallback callback = mock( RequestCallback.class );
-    final Request request = mock( Request.class );
-    final Response response = mock( Response.class );
-    when( response.getHeader( SharedConstants.REQUEST_COMPLETE_HEADER ) ).thenReturn( "1" );
-    doAnswer( invocation ->
-              {
-                final RequestCallback innerCallback = (RequestCallback) invocation.getArguments()[ 0 ];
-                innerCallback.onResponseReceived( request, response );
-                return null;
-              } ).when( rb ).setCallback( any( RequestCallback.class ) );
-    new ReplicantRpcRequestBuilder( sessionContext ).doSetCallback( rb, callback );
-    verify( callback ).onResponseReceived( request, response );
-    verify( rb ).setHeader( refEq( SharedConstants.CONNECTION_ID_HEADER ), refEq( connection.getConnectionId() ) );
-    verify( rb ).setHeader( refEq( SharedConstants.REQUEST_ID_HEADER ),
-                            refEq( String.valueOf( requestEntry.getRequestId() ) ) );
+    TestReplicantRpcRequestBuilder( @Nonnull final replicant.Request request )
+    {
+      _request = Objects.requireNonNull( request );
+    }
 
-    assertEquals( requestEntry.isExpectingResults(), false );
-  }
-
-  @Test
-  public void requestIDSet_withSuccessAndIncomplete()
-    throws Exception
-  {
-    final Connection connection = new Connection( TestConnector.create(), ValueUtil.randomString() );
-    final RequestEntry requestEntry = connection.newRequest( "", null );
-    final SessionContext sessionContext = new SessionContext();
-    sessionContext.setConnection( connection );
-    setRequest( sessionContext, requestEntry );
-    final RequestBuilder rb = mock( RequestBuilder.class );
-    final RequestCallback callback = mock( RequestCallback.class );
-    final Request request = mock( Request.class );
-    final Response response = mock( Response.class );
-    when( response.getHeader( SharedConstants.REQUEST_COMPLETE_HEADER ) ).thenReturn( "0" );
-    doAnswer( invocation ->
-              {
-                final RequestCallback innerCallback = (RequestCallback) invocation.getArguments()[ 0 ];
-                innerCallback.onResponseReceived( request, response );
-                return null;
-              } ).when( rb ).setCallback( any( RequestCallback.class ) );
-    new ReplicantRpcRequestBuilder( sessionContext ).doSetCallback( rb, callback );
-    verify( callback ).onResponseReceived( request, response );
-    verify( rb ).setHeader( refEq( SharedConstants.CONNECTION_ID_HEADER ), refEq( connection.getConnectionId() ) );
-    verify( rb ).setHeader( refEq( SharedConstants.REQUEST_ID_HEADER ),
-                            refEq( String.valueOf( requestEntry.getRequestId() ) ) );
-
-    assertEquals( requestEntry.isCompletionDataPresent(), false );
-    assertEquals( requestEntry.isExpectingResults(), true );
-  }
-
-  @Test
-  public void requestIDSet_withFailure()
-    throws Exception
-  {
-    final Connection connection = new Connection( TestConnector.create(), ValueUtil.randomString() );
-    final RequestEntry requestEntry = connection.newRequest( "", null );
-    final SessionContext sessionContext = new SessionContext();
-    sessionContext.setConnection( connection );
-    setRequest( sessionContext, requestEntry );
-    final RequestBuilder rb = mock( RequestBuilder.class );
-    final RequestCallback callback = mock( RequestCallback.class );
-    final Request request = mock( Request.class );
-    final Throwable exception = new Throwable();
-    doAnswer( invocation ->
-              {
-                final RequestCallback innerCallback = (RequestCallback) invocation.getArguments()[ 0 ];
-                innerCallback.onError( request, exception );
-                return null;
-              } ).when( rb ).setCallback( any( RequestCallback.class ) );
-    new ReplicantRpcRequestBuilder( sessionContext ).doSetCallback( rb, callback );
-    verify( callback ).onError( request, exception );
-    verify( rb ).setHeader( refEq( SharedConstants.CONNECTION_ID_HEADER ), refEq( connection.getConnectionId() ) );
-    verify( rb ).setHeader( refEq( SharedConstants.REQUEST_ID_HEADER ),
-                            refEq( String.valueOf( requestEntry.getRequestId() ) ) );
-
-    assertEquals( requestEntry.isExpectingResults(), false );
-  }
-
-  private void setRequest( final SessionContext sessionContext, final RequestEntry requestEntry )
-    throws Exception
-  {
-    toField( SessionContext.class, "_request" ).set( sessionContext, requestEntry );
+    @Nonnull
+    @Override
+    protected replicant.Request getRequest()
+    {
+      return _request;
+    }
   }
 }
