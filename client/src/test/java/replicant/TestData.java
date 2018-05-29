b8a2fee49915251e9269c82f2af4be3dfa8e545f
Peter Donald <peter@realityforge.org>
2018-05-29 15:31:06 +1000

    Remove some unused code

--- client/src/test/java/replicant/TestData.java
+++ client/src/test/java/replicant/TestData.java
@@ -6,13 +6,11 @@ final class TestData
     new ChannelSchema( 0, "MetaData", true, ChannelSchema.FilterType.NONE, null, true, true );
   private static final ChannelSchema EVENT_CHANNEL =
     new ChannelSchema( 1, "Event", false, ChannelSchema.FilterType.NONE, null, false, true );
-  private static final ChannelSchema EVENTS_LIST_CHANNEL =
-    new ChannelSchema( 2, "EventsList", true, ChannelSchema.FilterType.STATIC, null, false, true );
 
   static final SystemSchema ROSE_SYSTEM =
     new SystemSchema( 1,
                       "Rose",
-                      new ChannelSchema[]{ METADATA_CHANNEL, EVENT_CHANNEL, EVENTS_LIST_CHANNEL },
+                      new ChannelSchema[]{ METADATA_CHANNEL, EVENT_CHANNEL },
                       new EntitySchema[ 0 ] );
 
   private TestData()
