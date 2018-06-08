aee5f92fc7f350fabb387ae67bd20bb0322be324
Peter Donald <peter@realityforge.org>
2018-06-08 14:50:54 +1000

    Add classes to represent relationships

---
+++ client/src/main/java/org/realityforge/replicant/client/HasZeroOrOneInverseRelationship.java
@@ -0,0 +1,38 @@
+package org.realityforge.replicant.client;
+
+import arez.annotations.ArezComponent;
+import arez.component.AbstractEntityReference;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+@ArezComponent( observable = false )
+public abstract class HasZeroOrOneInverseRelationship<T>
+  extends AbstractEntityReference<T>
+{
+  @Nonnull
+  public static <T> HasZeroOrOneInverseRelationship<T> create()
+  {
+    return new Arez_HasZeroOrOneInverseRelationship<>();
+  }
+
+  public void link( @Nonnull final T entity )
+  {
+    setEntity( entity );
+  }
+
+  public void delink( @Nonnull final T entity )
+  {
+    setEntity( null );
+  }
+
+  @Nullable
+  public final T getReference()
+  {
+    return getEntity();
+  }
+
+  public final boolean hasReference()
+  {
+    return hasEntity();
+  }
+}
