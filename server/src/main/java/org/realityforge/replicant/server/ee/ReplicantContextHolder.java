8d89b1a10e8a9863350d510db461a5a69ed6d55a
Peter Donald <peter@realityforge.org>
2018-06-14 11:52:25 +1000

    Support nulls in puts which just results in the samw behaviour as remove

--- server/src/main/java/org/realityforge/replicant/server/ee/ReplicantContextHolder.java
+++ server/src/main/java/org/realityforge/replicant/server/ee/ReplicantContextHolder.java
@@ -27,9 +27,16 @@ public final class ReplicantContextHolder
    * @param key  the key.
    * @param data the data.
    */
-  public static void put( @Nonnull final String key, @Nonnull final Serializable data )
+  public static void put( @Nonnull final String key, @Nullable final Serializable data )
   {
-    getRawContext().put( key, data );
+    if ( null == data )
+    {
+      getRawContext().remove( key );
+    }
+    else
+    {
+      getRawContext().put( key, data );
+    }
   }
 
   /**
