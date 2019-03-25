57714f39593336c10ec1772fa7cbf52ff1460a22
Peter Donald <peter@realityforge.org>
2019-03-25 17:36:21 +1100

    Add bulk unsubscribe message

---
+++ client/src/main/java/replicant/messages/BulkUnsubscribeMessage.java
@@ -0,0 +1,27 @@
+package replicant.messages;
+
+import java.util.Objects;
+import javax.annotation.Nonnull;
+import jsinterop.annotations.JsOverlay;
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsType;
+
+@JsType( isNative = true, namespace = JsPackage.GLOBAL, name = "Object" )
+@SuppressWarnings( { "NullableProblems", "unused" } )
+public final class BulkUnsubscribeMessage
+  extends ClientToServerMessage
+{
+  @Nonnull
+  private String[] channels;
+
+  @JsOverlay
+  @Nonnull
+  public static BulkUnsubscribeMessage create( final int req, @Nonnull final String[] channels )
+  {
+    final BulkUnsubscribeMessage message = new BulkUnsubscribeMessage();
+    message.type = "bulk-unsub";
+    message.req = req;
+    message.channels = Objects.requireNonNull( channels );
+    return message;
+  }
+}
