c0936248f6f83988b67d68e352a51b084e058f7e
Peter Donald <peter@realityforge.org>
2019-12-09 22:26:54 +1100

    Cleanup tests

--- server/src/test/java/org/realityforge/replicant/server/ChangeAccumulatorTest.java
+++ server/src/test/java/org/realityforge/replicant/server/ChangeAccumulatorTest.java
@@ -1,6 +1,5 @@
 package org.realityforge.replicant.server;
 
-import java.io.Serializable;
 import java.util.Collections;
 import java.util.Map;
 import javax.json.Json;
@@ -17,7 +16,7 @@ public class ChangeAccumulatorTest
   @Test
   public void basicOperation()
   {
-    final ReplicantSession c = new ReplicantSession( null, "s1" );
+    final ReplicantSession session = new ReplicantSession( null, ValueUtil.randomString() );
     final ChangeAccumulator accumulator = new ChangeAccumulator();
 
     final int id = 17;
@@ -27,14 +26,14 @@ public class ChangeAccumulatorTest
 
     final int channelId = 1;
     final Integer subChannelId = 2;
+    final Integer requestId = 3;
 
-    accumulator.addChange( c, new Change( message, channelId, subChannelId ) );
-    final Integer requestId = ValueUtil.randomInt();
-    final boolean impactsInitiator = accumulator.complete( "s1", requestId );
+    accumulator.addChange( session, new Change( message, channelId, subChannelId ) );
+    final boolean impactsInitiator = accumulator.complete( session, requestId );
 
     assertTrue( impactsInitiator );
-    assertEquals( c.getQueue().size(), 1 );
-    final Packet packet = c.getQueue().nextPacketToProcess();
+    assertEquals( session.getQueue().size(), 1 );
+    final Packet packet = session.getQueue().nextPacketToProcess();
     final Change change = packet.getChangeSet().getChanges().iterator().next();
     assertEquals( change.getKey(), "42#17" );
     assertEquals( change.getEntityMessage().getId(), id );
@@ -45,13 +44,13 @@ public class ChangeAccumulatorTest
     assertEquals( channels.get( channelId ), subChannelId );
 
     accumulator.complete( null, null );
-    assertEquals( c.getQueue().size(), 1 );
+    assertEquals( session.getQueue().size(), 1 );
   }
 
   @Test
   public void addEntityMessages()
   {
-    final ReplicantSession c = new ReplicantSession( null, "s1" );
+    final ReplicantSession session = new ReplicantSession( null, ValueUtil.randomString() );
     final ChangeAccumulator accumulator = new ChangeAccumulator();
 
     final int id = 17;
@@ -59,88 +58,98 @@ public class ChangeAccumulatorTest
 
     final EntityMessage message = MessageTestUtil.createMessage( id, typeID, 0, "r1", "r2", "a1", "a2" );
 
-    accumulator.addChanges( c, Collections.singletonList( new Change( message, 1, 0 ) ) );
+    final int channelId = 1;
+    final Integer subChannelId = 0;
+    final Integer requestId = 3;
 
-    assertEquals( accumulator.getChangeSet( c ).getChanges().size(), 1 );
+    accumulator.addChanges( session, Collections.singletonList( new Change( message, channelId, subChannelId ) ) );
 
-    final Integer requestId = ValueUtil.randomInt();
-    final boolean impactsInitiator = accumulator.complete( "s1", requestId );
+    assertEquals( accumulator.getChangeSet( session ).getChanges().size(), 1 );
 
-    assertEquals( accumulator.getChangeSet( c ).getChanges().size(), 0 );
+    final boolean impactsInitiator = accumulator.complete( session, requestId );
+
+    assertEquals( accumulator.getChangeSet( session ).getChanges().size(), 0 );
 
     assertTrue( impactsInitiator );
-    assertEquals( c.getQueue().size(), 1 );
-    final Packet packet = c.getQueue().nextPacketToProcess();
+    assertEquals( session.getQueue().size(), 1 );
+    final Packet packet = session.getQueue().nextPacketToProcess();
     assertEquals( packet.getChangeSet().getChanges().iterator().next().getEntityMessage().getId(), id );
     assertEquals( packet.getRequestId(), requestId );
 
     accumulator.complete( null, null );
-    assertEquals( c.getQueue().size(), 1 );
+    assertEquals( session.getQueue().size(), 1 );
   }
 
   @Test
   public void addActions()
   {
-    final ReplicantSession c = new ReplicantSession( null, "s1" );
+    final ReplicantSession session = new ReplicantSession( null, ValueUtil.randomString() );
     final ChangeAccumulator accumulator = new ChangeAccumulator();
 
     final JsonObject filter = Json.createBuilderFactory( null ).createObjectBuilder().build();
-    accumulator.addActions( c,
-                            Collections.singletonList( new ChannelAction( new ChannelAddress( 1, 2 ),
+    final int channelId = 1;
+    final Integer subChannelId = 2;
+    final Integer requestId = 1;
+
+    accumulator.addActions( session,
+                            Collections.singletonList( new ChannelAction( new ChannelAddress( channelId, subChannelId ),
                                                                           Action.ADD,
                                                                           filter ) ) );
 
-    assertEquals( accumulator.getChangeSet( c ).getChannelActions().size(), 1 );
+    assertEquals( accumulator.getChangeSet( session ).getChannelActions().size(), 1 );
 
-    final Integer requestId = ValueUtil.randomInt();
-    final boolean impactsInitiator = accumulator.complete( "s1", requestId );
+    final boolean impactsInitiator = accumulator.complete( session, requestId );
 
-    assertEquals( accumulator.getChangeSet( c ).getChannelActions().size(), 0 );
+    assertEquals( accumulator.getChangeSet( session ).getChannelActions().size(), 0 );
 
     assertTrue( impactsInitiator );
-    assertEquals( c.getQueue().size(), 1 );
-    final Packet packet = c.getQueue().nextPacketToProcess();
+    assertEquals( session.getQueue().size(), 1 );
+    final Packet packet = session.getQueue().nextPacketToProcess();
     final ChannelAction action = packet.getChangeSet().getChannelActions().iterator().next();
-    assertEquals( action.getAddress().getChannelId(), 1 );
+    assertEquals( action.getAddress().getChannelId(), channelId );
     assertEquals( action.getAction(), Action.ADD );
     assertEquals( action.getFilter(), filter );
     assertEquals( packet.getRequestId(), requestId );
 
-    assertEquals( c.getQueue().size(), 1 );
+    assertEquals( session.getQueue().size(), 1 );
     accumulator.complete( null, null );
-    assertEquals( c.getQueue().size(), 1 );
+    assertEquals( session.getQueue().size(), 1 );
   }
 
   @Test
-  public void basicOperation_whereSessionIDDifferent()
+  public void basicOperation_whereSessionDifferent()
   {
-    final ReplicantSession c = new ReplicantSession( null, "s1" );
+    final ReplicantSession session = new ReplicantSession( null, ValueUtil.randomString() );
+    final ReplicantSession session2 = new ReplicantSession( null, ValueUtil.randomString() );
     final ChangeAccumulator accumulator = new ChangeAccumulator();
 
     final EntityMessage message = MessageTestUtil.createMessage( 17, 42, 0, "r1", "r2", "a1", "a2" );
 
-    accumulator.addChange( c, new Change( message, 1, 0 ) );
-    final Integer requestId = ValueUtil.randomInt();
-    final boolean impactsInitiator = accumulator.complete( "s2", requestId );
+    final int channelId = 1;
+    final Integer subChannelId = 0;
+
+    accumulator.addChange( session, new Change( message, channelId, subChannelId ) );
+    final boolean impactsInitiator = accumulator.complete( session2, 1 );
 
     assertFalse( impactsInitiator );
 
-    assertEquals( c.getQueue().size(), 1 );
-    assertNull( c.getQueue().nextPacketToProcess().getRequestId() );
+    assertEquals( session.getQueue().size(), 1 );
+    assertNull( session.getQueue().nextPacketToProcess().getRequestId() );
   }
 
   @Test
   public void basicOperation_whereNoMessagesSentToInitiator()
   {
-    final ReplicantSession c = new ReplicantSession( null, "s1" );
+    final ReplicantSession session = new ReplicantSession( null, ValueUtil.randomString() );
     final ChangeAccumulator accumulator = new ChangeAccumulator();
 
-    accumulator.getChangeSet( c );
-    final Integer requestId = ValueUtil.randomInt();
-    final boolean impactsInitiator = accumulator.complete( "s1", requestId );
+    final Integer requestId = 3;
+
+    accumulator.getChangeSet( session );
+    final boolean impactsInitiator = accumulator.complete( session, requestId );
 
     assertFalse( impactsInitiator );
 
-    assertEquals( c.getQueue().size(), 0 );
+    assertEquals( session.getQueue().size(), 0 );
   }
 }
