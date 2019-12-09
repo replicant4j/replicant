df02b257f3477d55929ef44d75ea098b15eb8188
Peter Donald <peter@realityforge.org>
2019-12-09 23:06:55 +1100

    Remove unused code

--- server/src/main/java/org/realityforge/replicant/server/ee/rest/AbstractReplicantRestService.java
+++ server/src/main/java/org/realityforge/replicant/server/ee/rest/AbstractReplicantRestService.java
@@ -6,15 +6,12 @@ import javax.json.Json;
 import javax.json.stream.JsonGenerator;
 import javax.json.stream.JsonGeneratorFactory;
 import javax.ws.rs.core.Response;
-import javax.xml.datatype.DatatypeConfigurationException;
-import javax.xml.datatype.DatatypeFactory;
 
 /**
  * A base class for several of the rest services within the replicant project.
  */
 public abstract class AbstractReplicantRestService
 {
-  private DatatypeFactory _datatypeFactory;
   private JsonGeneratorFactory _factory;
 
   public void postConstruct()
@@ -22,15 +19,6 @@ public abstract class AbstractReplicantRestService
     final HashMap<String, Object> config = new HashMap<>();
     config.put( JsonGenerator.PRETTY_PRINTING, true );
     _factory = Json.createGeneratorFactory( config );
-
-    try
-    {
-      _datatypeFactory = DatatypeFactory.newInstance();
-    }
-    catch ( final DatatypeConfigurationException dtce )
-    {
-      throw new IllegalStateException( "Unable to initialize DatatypeFactory", dtce );
-    }
   }
 
   @Nonnull
@@ -45,11 +33,5 @@ public abstract class AbstractReplicantRestService
   protected JsonGeneratorFactory factory()
   {
     return _factory;
-  }
-
-  @Nonnull
-  protected DatatypeFactory datatypeFactory()
-  {
-    return _datatypeFactory;
   }
 }
