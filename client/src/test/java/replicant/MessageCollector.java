40ec57dde0cb65b36bbb764d90e5468bbf0a90ab
Peter Donald <peter@realityforge.org>
2019-12-17 09:55:21 +1100

    Stop recording callers

--- client/src/test/java/replicant/MessageCollector.java
+++ client/src/test/java/replicant/MessageCollector.java
@@ -23,6 +23,6 @@ public final class MessageCollector
     final String fixtureDir = System.getProperty( "replicant.diagnostic_messages_file" );
     assertNotNull( fixtureDir,
                    "Expected System.getProperty( \"replicant.diagnostic_messages_file\" ) to return location of diagnostic messages file" );
-    return new GuardMessageCollector( "Replicant", new File( fixtureDir ), saveIfChanged );
+    return new GuardMessageCollector( "Replicant", new File( fixtureDir ), saveIfChanged, true, false );
   }
 }
