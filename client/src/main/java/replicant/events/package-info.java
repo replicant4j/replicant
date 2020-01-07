e16ea47f6449c48bfa5caa4e847bec8a33695692
Peter Donald <peter@realityforge.org>
2020-01-07 16:56:23 +1100

    Add package in preparation for supporting legacy events

---
+++ client/src/main/java/replicant/events/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Notifications that appear to support event driven applications.
+ * Classes in this package are only used to support legacy applications and should not be used for new applications.
+ */
+package replicant.events;
