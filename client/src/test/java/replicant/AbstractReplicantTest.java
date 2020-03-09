dd86d2664abb9e03ca5c660ef134599111d13056
Peter Donald <peter@realityforge.org>
2020-03-09 15:25:51 +1100

    Optimize imports

--- client/src/test/java/replicant/AbstractReplicantTest.java
+++ client/src/test/java/replicant/AbstractReplicantTest.java
@@ -6,8 +6,6 @@ import java.lang.reflect.Field;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import org.realityforge.guiceyloops.shared.ValueUtil;
-import org.testng.IHookCallBack;
-import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
