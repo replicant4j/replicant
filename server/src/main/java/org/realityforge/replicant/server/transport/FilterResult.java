9e4228c62fa41ebd3f31091e42d32592603f284d
Peter Donald <peter@realityforge.org>
2021-09-16 15:53:37 +1000

    Add enum useful when performing filtering

---
+++ server/src/main/java/org/realityforge/replicant/server/transport/FilterResult.java
@@ -0,0 +1,20 @@
+package org.realityforge.replicant.server.transport;
+
+/**
+ * A enum controlling thw way we filter EntityMessage.
+ */
+public enum FilterResult
+{
+  /**
+   * Message should be kept
+   */
+  KEEP,
+  /**
+   * Message should be kept but translated into a "DELETE" as the entity has moved out of scope
+   */
+  DELETE,
+  /**
+   * Message should be filtered out
+   */
+  DISCARD
+}
