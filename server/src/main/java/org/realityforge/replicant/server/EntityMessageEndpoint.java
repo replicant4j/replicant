921cdf958a5f57c46106e8632cab2d2e464c974e
Peter Donald <peter@realityforge.org>
2025-02-03 09:46:36 +1100

    Rename parameter for clarity

--- server/src/main/java/org/realityforge/replicant/server/EntityMessageEndpoint.java
+++ server/src/main/java/org/realityforge/replicant/server/EntityMessageEndpoint.java
@@ -12,11 +12,11 @@ public interface EntityMessageEndpoint
    * @param sessionId the session that initiated the request that resulted in the changes, or null.
    * @param requestId the request that resulted in the changes, or null if no such request.
    * @param messages  the messages.
-   * @param changeSet the changes that are targeted for the session that initiated request.
+   * @param sessionChanges the changes that are targeted for the session that initiated request.
    * @return true if any messages were routed to the initiating session.
    */
   boolean saveEntityMessages( @Nullable String sessionId,
                               @Nullable Integer requestId,
                               @Nonnull Collection<EntityMessage> messages,
-                              @Nullable ChangeSet changeSet );
+                              @Nullable ChangeSet sessionChanges );
 }
