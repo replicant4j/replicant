b8a3670c6c7ec677b5738373449ada6bb2435b5d
Peter Donald <peter@realityforge.org>
2025-03-20 11:56:02 +1100

    Add transport message for execs

---
+++ client/src/main/java/replicant/messages/ExecMessage.java
@@ -0,0 +1,44 @@
+package replicant.messages;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import jsinterop.annotations.JsOverlay;
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsType;
+import replicant.shared.Messages;
+
+@JsType( isNative = true, namespace = JsPackage.GLOBAL, name = "Object" )
+@SuppressWarnings( { "NotNullFieldNotInitialized", "unused" } )
+public final class ExecMessage
+  extends ClientToServerMessage
+{
+  @JsOverlay
+  public static final String TYPE = Messages.C2S_Type.EXEC;
+
+  @Nonnull
+  private String command;
+  @Nullable
+  private Object payload;
+
+  @JsOverlay
+  @Nonnull
+  public static ExecMessage create( final int req, @Nonnull final String command, @Nullable final Object payload )
+  {
+    final ExecMessage message = new ExecMessage();
+    message.type = TYPE;
+    message.requestId = req;
+    message.command = command;
+    if ( null != payload )
+    {
+      message.payload = payload;
+    }
+    return message;
+  }
+
+  @JsOverlay
+  @Nonnull
+  public String getCommand()
+  {
+    return command;
+  }
+}
