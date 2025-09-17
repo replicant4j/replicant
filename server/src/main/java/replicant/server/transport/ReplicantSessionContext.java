b95994b9a57103d8595e23cf669e4482ee13a18c
Peter Donald <peter@realityforge.org>
2025-09-17 12:31:54 +1000

    Copy across some comments

--- server/src/main/java/replicant/server/transport/ReplicantSessionContext.java
+++ server/src/main/java/replicant/server/transport/ReplicantSessionContext.java
@@ -14,6 +14,9 @@ public interface ReplicantSessionContext
 
   void preSubscribe( @Nonnull ReplicantSession session, @Nonnull ChannelAddress address, @Nullable Object filter );
 
+  /**
+   * @return the cacheKey if any. The return value is ignored for non-cacheable channels.
+   */
   @Nonnull
   SubscribeResult collectDataForSubscribe( @Nonnull ChannelAddress address,
                                            @Nullable Object filter,
@@ -25,12 +28,22 @@ public interface ReplicantSessionContext
                                          @Nullable Object filter,
                                          @Nonnull ChangeSet changeSet );
 
+  /**
+   * This method is called in an attempt to use a more efficient method for bulk loading instance graphs.
+   * Subclasses may return false form this method, in which case collectDataForSubscribe will be called
+   * for each independent channel.
+   */
   void bulkCollectDataForSubscribe( @Nonnull ReplicantSession session,
                                     @Nonnull List<ChannelAddress> addresses,
                                     @Nullable Object filter,
                                     @Nonnull ChangeSet changeSet,
                                     boolean isExplicitSubscribe );
 
+  /**
+   * Hook method by which efficient bulk collection of data for subscription updates can occur.
+   * It is expected that the hook does everything including updating SubscriptionEntry with new
+   * filter, adding graph links etc.
+   */
   void bulkCollectDataForSubscriptionUpdate( @Nonnull ReplicantSession session,
                                              @Nonnull List<ChannelAddress> addresses,
                                              @Nullable Object originalFilter,
