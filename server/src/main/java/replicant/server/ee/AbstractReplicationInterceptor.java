2b0776487eb5e004dd1e752d5d8f2bdc71b1c6d6
Peter Donald <peter@realityforge.org>
2025-09-23 15:09:44 +1000

    Inline logic

--- server/src/main/java/replicant/server/ee/AbstractReplicationInterceptor.java
+++ server/src/main/java/replicant/server/ee/AbstractReplicationInterceptor.java
@@ -2,16 +2,19 @@ package replicant.server.ee;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.util.concurrent.Callable;
 import javax.annotation.Nonnull;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.InvocationContext;
+import replicant.server.ServerConstants;
+import replicant.server.transport.ReplicantSession;
+import replicant.server.transport.ReplicantSessionManager;
 
 /**
  * A base class for an interceptor that should be applied to all services that need to send out EntityChange messages
  * on completion.
  */
 public abstract class AbstractReplicationInterceptor
-  extends AbstractInvocationAdapter
 {
   @AroundInvoke
   public Object businessIntercept( final InvocationContext context )
@@ -35,4 +38,29 @@ public abstract class AbstractReplicationInterceptor
     }
     return "Unknown";
   }
+
+  private <T> T invokeAction( @Nonnull final String key, @Nonnull final Callable<T> action )
+    throws Exception
+  {
+    final String sessionId = (String) ReplicantContextHolder.remove( ServerConstants.SESSION_ID_KEY );
+    final Integer requestId = (Integer) ReplicantContextHolder.remove( ServerConstants.REQUEST_ID_KEY );
+    final ReplicantSession session = null != sessionId ? getReplicantSessionManager().getSession( sessionId ) : null;
+    ReplicantContextHolder.clean();
+    try
+    {
+      final Callable<T> actionWrapper = () -> {
+        final T result = action.call();
+        ReplicantContextHolder.clean();
+        return result;
+      };
+      return getReplicantSessionManager().runRequest( key, session, requestId, actionWrapper );
+    }
+    finally
+    {
+      ReplicantContextHolder.clean();
+    }
+  }
+
+  @Nonnull
+  protected abstract ReplicantSessionManager getReplicantSessionManager();
 }
