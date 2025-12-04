5ee222a4118233177f8c0ffa774f476d64840671
Peter Donald <peter@realityforge.org>
2025-12-04 13:11:36 +1100

    Mark class as final

--- client/src/test/java/replicant/ChannelAddressTest.java
+++ client/src/test/java/replicant/ChannelAddressTest.java
@@ -4,7 +4,7 @@ import org.realityforge.guiceyloops.shared.ValueUtil;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
-public class ChannelAddressTest
+public final class ChannelAddressTest
   extends AbstractReplicantTest
 {
   @Test
