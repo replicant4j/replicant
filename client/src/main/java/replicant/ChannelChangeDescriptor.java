e732e132b9e8c18d2451e2fe940e430cd0733a8d
Peter Donald <peter@realityforge.org>
2025-12-04 13:28:37 +1100

    Raise exception when parsing fails

--- client/src/main/java/replicant/ChannelChangeDescriptor.java
+++ client/src/main/java/replicant/ChannelChangeDescriptor.java
@@ -23,9 +23,16 @@ final class ChannelChangeDescriptor
   @Nonnull
   static ChannelChangeDescriptor from( final int schema, @Nonnull final String channelAction )
   {
-    final Type type = actionToType( channelAction );
-    final ChannelAddress address = ChannelAddress.parse( schema, channelAction.substring( 1 ) );
-    return new ChannelChangeDescriptor( type, address, null );
+    try
+    {
+      final String descriptor = channelAction.substring( 1 );
+      final ChannelAddress address = ChannelAddress.parse( schema, descriptor );
+      return new ChannelChangeDescriptor( actionToType( channelAction ), address, null );
+    }
+    catch ( final Throwable t )
+    {
+      throw new IllegalStateException( "Failed to parse channel action '" + channelAction + "'", t );
+    }
   }
 
   @Nonnull
@@ -37,11 +44,19 @@ final class ChannelChangeDescriptor
   @Nonnull
   private static Type actionToType( @Nonnull final String channelAction )
   {
+    assert !channelAction.isEmpty();
     final char commandCode = channelAction.charAt( 0 );
-    return Messages.Update.CHANNEL_ACTION_ADD == commandCode ? Type.ADD :
-           Messages.Update.CHANNEL_ACTION_REMOVE == commandCode ? Type.REMOVE :
-           Messages.Update.CHANNEL_ACTION_UPDATE == commandCode ? Type.UPDATE :
-           Type.DELETE;
+    final Type type =
+      Messages.Update.CHANNEL_ACTION_ADD == commandCode ? Type.ADD :
+      Messages.Update.CHANNEL_ACTION_REMOVE == commandCode ? Type.REMOVE :
+      Messages.Update.CHANNEL_ACTION_UPDATE == commandCode ? Type.UPDATE :
+      Messages.Update.CHANNEL_ACTION_DELETE == commandCode ? Type.DELETE :
+      null;
+    if ( null == type )
+    {
+      throw new IllegalArgumentException( "Unknown channel action '" + channelAction + "'" );
+    }
+    return type;
   }
 
   private ChannelChangeDescriptor( @Nonnull final Type type,
