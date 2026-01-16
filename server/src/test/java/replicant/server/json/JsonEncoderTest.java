3846539772b34290729be93b5ac127cd0754e304
Peter Donald <peter@realityforge.org>
2026-01-17 07:11:25 +1100

    Prefer var

--- server/src/test/java/replicant/server/json/JsonEncoderTest.java
+++ server/src/test/java/replicant/server/json/JsonEncoderTest.java
@@ -30,8 +30,8 @@ public final class JsonEncoderTest
   @Test
   public void encodeAllData()
   {
-    final int id = 17;
-    final int typeID = 42;
+    final var id = 17;
+    final var typeID = 42;
     final Calendar calendar = Calendar.getInstance();
     calendar.set( Calendar.YEAR, 2001 );
     calendar.set( Calendar.MONTH, Calendar.JULY );
@@ -44,27 +44,27 @@ public final class JsonEncoderTest
 
     final Date date = calendar.getTime();
 
-    final EntityMessage message = MessageTestUtil.createMessage( id, typeID, 0, "r1", "r2", "a1", "a2" );
+    final var message = MessageTestUtil.createMessage( id, typeID, 0, "r1", "r2", "a1", "a2" );
     final Map<String, Serializable> values = message.getAttributeValues();
     assertNotNull( values );
     values.put( "key3", date );
 
-    final int requestId = 1;
+    final var requestId = 1;
     final JsonValue response =
       Json.createArrayBuilder().add( 17 ).add( 42 ).build();
 
-    final String etag = "#1";
-    final JsonObject filter = Json.createBuilderFactory( null ).createObjectBuilder().add( "a", "b" ).build();
+    final var etag = "#1";
+    final var filter = Json.createBuilderFactory( null ).createObjectBuilder().add( "a", "b" ).build();
 
     final Change change = new Change( message );
     change.getChannels().add( new ChannelAddress( 1, null ) );
     change.getChannels().add( new ChannelAddress( 2, 42 ) );
     change.getChannels().add( new ChannelAddress( 3, 73 ) );
-    final ChangeSet cs = new ChangeSet();
+    final var cs = new ChangeSet();
     cs.merge( change );
     cs.mergeAction( new ChannelAction( new ChannelAddress( 45, 77 ), Action.UPDATE, filter ) );
-    final String encoded = JsonEncoder.encodeChangeSet( requestId, response, etag, cs );
-    final JsonObject changeSet = toJsonObject( encoded );
+    final var encoded = JsonEncoder.encodeChangeSet( requestId, response, etag, cs );
+    final var changeSet = toJsonObject( encoded );
 
     assertNotNull( changeSet );
 
@@ -75,15 +75,15 @@ public final class JsonEncoderTest
     assertEquals( jsonResponse.getInt( 1 ), 42 );
     assertEquals( changeSet.getString( Messages.S2C_Common.ETAG ), etag );
 
-    final JsonObject action = changeSet.getJsonArray( Messages.Update.FILTERED_CHANNEL_ACTIONS ).getJsonObject( 0 );
+    final var action = changeSet.getJsonArray( Messages.Update.FILTERED_CHANNEL_ACTIONS ).getJsonObject( 0 );
     assertEquals( action.getString( Messages.Common.CHANNEL ), "=45.77" );
     assertEquals( action.getJsonObject( Messages.Update.FILTER ).toString(), filter.toString() );
 
-    final JsonObject object = changeSet.getJsonArray( Messages.Update.CHANGES ).getJsonObject( 0 );
+    final var object = changeSet.getJsonArray( Messages.Update.CHANGES ).getJsonObject( 0 );
 
     assertEquals( object.getString( Messages.Update.ENTITY_ID ), "42.17" );
 
-    final JsonObject data = object.getJsonObject( Messages.Update.DATA );
+    final var data = object.getJsonObject( Messages.Update.DATA );
     assertNotNull( data );
     assertEquals( data.getString( MessageTestUtil.ATTR_KEY1 ), "a1" );
     assertEquals( data.getString( MessageTestUtil.ATTR_KEY2 ), "a2" );
@@ -92,11 +92,11 @@ public final class JsonEncoderTest
     final JsonArray channels = object.getJsonArray( Messages.Update.CHANNELS );
     assertNotNull( channels );
     assertEquals( channels.size(), 3 );
-    final String channel1 = channels.getString( 0 );
+    final var channel1 = channels.getString( 0 );
     assertNotNull( channel1 );
-    final String channel2 = channels.getString( 1 );
+    final var channel2 = channels.getString( 1 );
     assertNotNull( channel2 );
-    final String channel3 = channels.getString( 2 );
+    final var channel3 = channels.getString( 2 );
     assertNotNull( channel3 );
 
     assertEquals( channel1, "1" );
@@ -107,19 +107,19 @@ public final class JsonEncoderTest
   @Test
   public void encodeChangeSetFromEntityMessages_deleteMessage()
   {
-    final int id = 17;
-    final int typeID = 42;
+    final var id = 17;
+    final var typeID = 42;
 
-    final EntityMessage message = MessageTestUtil.createMessage( id, typeID, 0, "r1", "r2", null, null );
+    final var message = MessageTestUtil.createMessage( id, typeID, 0, "r1", "r2", null, null );
 
-    final ChangeSet cs = new ChangeSet();
+    final var cs = new ChangeSet();
     cs.merge( new Change( message ) );
-    final String encoded = JsonEncoder.encodeChangeSet( null, null, null, cs );
-    final JsonObject changeSet = toJsonObject( encoded );
+    final var encoded = JsonEncoder.encodeChangeSet( null, null, null, cs );
+    final var changeSet = toJsonObject( encoded );
 
     assertNotNull( changeSet );
 
-    final JsonObject object = changeSet.getJsonArray( Messages.Update.CHANGES ).getJsonObject( 0 );
+    final var object = changeSet.getJsonArray( Messages.Update.CHANGES ).getJsonObject( 0 );
 
     assertEquals( object.getString( Messages.Update.ENTITY_ID ), "42.17" );
 
@@ -129,8 +129,8 @@ public final class JsonEncoderTest
   @Test
   public void encodeChangeSet_empty()
   {
-    final ChangeSet cs = new ChangeSet();
-    final JsonObject changeSet = toJsonObject( JsonEncoder.encodeChangeSet( null, null, null, cs ) );
+    final var cs = new ChangeSet();
+    final var changeSet = toJsonObject( JsonEncoder.encodeChangeSet( null, null, null, cs ) );
 
     assertNotNull( changeSet );
     assertEquals( changeSet.getString( Messages.Common.TYPE ), Messages.S2C_Type.UPDATE );
@@ -150,9 +150,9 @@ public final class JsonEncoderTest
   @Test
   public void action_WithNoFilter()
   {
-    final ChangeSet cs = new ChangeSet();
+    final var cs = new ChangeSet();
     cs.mergeAction( new ChannelAction( new ChannelAddress( 45, null ), Action.ADD, null ) );
-    final JsonObject changeSet = toJsonObject( JsonEncoder.encodeChangeSet( null, null, null, cs ) );
+    final var changeSet = toJsonObject( JsonEncoder.encodeChangeSet( null, null, null, cs ) );
     assertNotNull( changeSet );
 
     assertEquals( changeSet.getJsonArray( Messages.Update.CHANNEL_ACTIONS ).getString( 0 ), "+45" );
@@ -161,9 +161,9 @@ public final class JsonEncoderTest
   @Test
   public void channelAction_DELETE()
   {
-    final ChangeSet cs = new ChangeSet();
+    final var cs = new ChangeSet();
     cs.mergeAction( new ChannelAction( new ChannelAddress( 45, null ), Action.DELETE, null ) );
-    final JsonObject changeSet = toJsonObject( JsonEncoder.encodeChangeSet( null, null, null, cs ) );
+    final var changeSet = toJsonObject( JsonEncoder.encodeChangeSet( null, null, null, cs ) );
     assertNotNull( changeSet );
 
     assertEquals( changeSet.getJsonArray( Messages.Update.CHANNEL_ACTIONS ).getString( 0 ), "!45" );
@@ -172,15 +172,15 @@ public final class JsonEncoderTest
   @Test
   public void mixedChannelActions()
   {
-    final ChangeSet cs = new ChangeSet();
+    final var cs = new ChangeSet();
     cs.mergeAction( new ChannelAction( new ChannelAddress( 1, null ), Action.ADD, null ) );
     cs.mergeAction( new ChannelAction( new ChannelAddress( 2, 5 ), Action.REMOVE, null ) );
     cs.mergeAction( new ChannelAction( new ChannelAddress( 3, 7, "inst" ), Action.UPDATE, null ) );
 
-    final JsonObject filter = Json.createObjectBuilder().add( "a", "b" ).build();
+    final var filter = Json.createObjectBuilder().add( "a", "b" ).build();
     cs.mergeAction( new ChannelAction( new ChannelAddress( 4, 9 ), Action.ADD, filter ) );
 
-    final JsonObject changeSet = toJsonObject( JsonEncoder.encodeChangeSet( null, null, null, cs ) );
+    final var changeSet = toJsonObject( JsonEncoder.encodeChangeSet( null, null, null, cs ) );
     assertNotNull( changeSet );
 
     final JsonArray actions = changeSet.getJsonArray( Messages.Update.CHANNEL_ACTIONS );
@@ -189,7 +189,7 @@ public final class JsonEncoderTest
     assertEquals( actions.getString( 1 ), "-2.5" );
     assertEquals( actions.getString( 2 ), "=3.7#inst" );
 
-    final JsonObject filteredAction =
+    final var filteredAction =
       changeSet.getJsonArray( Messages.Update.FILTERED_CHANNEL_ACTIONS ).getJsonObject( 0 );
     assertEquals( filteredAction.getString( Messages.Common.CHANNEL ), "+4.9" );
     assertEquals( filteredAction.getJsonObject( Messages.Update.FILTER ).toString(), filter.toString() );
@@ -209,8 +209,8 @@ public final class JsonEncoderTest
     calendar.set( Calendar.MILLISECOND, 0 );
     final Date date = calendar.getTime();
 
-    final HashMap<String, Serializable> routingKeys = new HashMap<>();
-    final HashMap<String, Serializable> attributeData = new HashMap<>();
+    final var routingKeys = new HashMap<String, Serializable>();
+    final var attributeData = new HashMap<String, Serializable>();
     attributeData.put( "s", "text" );
     attributeData.put( "i", 12 );
     attributeData.put( "f", 1.5f );
@@ -218,13 +218,13 @@ public final class JsonEncoderTest
     attributeData.put( "d", date );
     attributeData.put( "n", null );
 
-    final EntityMessage message = new EntityMessage( 1, 2, 0, routingKeys, attributeData, null );
-    final ChangeSet cs = new ChangeSet();
+    final var message = new EntityMessage( 1, 2, 0, routingKeys, attributeData, null );
+    final var cs = new ChangeSet();
     cs.merge( new Change( message ) );
 
-    final JsonObject changeSet = toJsonObject( JsonEncoder.encodeChangeSet( null, null, null, cs ) );
-    final JsonObject change = changeSet.getJsonArray( Messages.Update.CHANGES ).getJsonObject( 0 );
-    final JsonObject data = change.getJsonObject( Messages.Update.DATA );
+    final var changeSet = toJsonObject( JsonEncoder.encodeChangeSet( null, null, null, cs ) );
+    final var change = changeSet.getJsonArray( Messages.Update.CHANGES ).getJsonObject( 0 );
+    final var data = change.getJsonObject( Messages.Update.DATA );
 
     assertEquals( data.getString( "s" ), "text" );
     assertEquals( data.getInt( "i" ), 12 );
@@ -238,17 +238,17 @@ public final class JsonEncoderTest
   @Test
   public void encodeChangeSet_channelDescriptors_includeFilterInstanceId()
   {
-    final HashMap<String, Serializable> routingKeys = new HashMap<>();
-    final HashMap<String, Serializable> attributeData = new HashMap<>();
+    final var routingKeys = new HashMap<String, Serializable>();
+    final var attributeData = new HashMap<String, Serializable>();
     attributeData.put( "x", "y" );
-    final EntityMessage message = new EntityMessage( 1, 2, 0, routingKeys, attributeData, null );
+    final var message = new EntityMessage( 1, 2, 0, routingKeys, attributeData, null );
     final Change change = new Change( message );
     change.getChannels().add( new ChannelAddress( 7, null, "fi" ) );
     change.getChannels().add( new ChannelAddress( 8, 3, "fi-2" ) );
-    final ChangeSet cs = new ChangeSet();
+    final var cs = new ChangeSet();
     cs.merge( change );
 
-    final JsonObject changeSet = toJsonObject( JsonEncoder.encodeChangeSet( null, null, null, cs ) );
+    final var changeSet = toJsonObject( JsonEncoder.encodeChangeSet( null, null, null, cs ) );
     final JsonArray channels =
       changeSet.getJsonArray( Messages.Update.CHANGES ).getJsonObject( 0 ).getJsonArray( Messages.Update.CHANNELS );
 
@@ -260,14 +260,14 @@ public final class JsonEncoderTest
   @Test
   public void encodeChangeSet_rejectsUnsupportedValues()
   {
-    final HashMap<String, Serializable> routingKeys = new HashMap<>();
-    final HashMap<String, Serializable> attributeData = new HashMap<>();
+    final var routingKeys = new HashMap<String, Serializable>();
+    final var attributeData = new HashMap<String, Serializable>();
     attributeData.put( "bad", (byte) 1 );
-    final EntityMessage message = new EntityMessage( 1, 2, 0, routingKeys, attributeData, null );
-    final ChangeSet cs = new ChangeSet();
+    final var message = new EntityMessage( 1, 2, 0, routingKeys, attributeData, null );
+    final var cs = new ChangeSet();
     cs.merge( new Change( message ) );
 
-    final IllegalStateException exception =
+    final var exception =
       expectThrows( IllegalStateException.class, () -> JsonEncoder.encodeChangeSet( null, null, null, cs ) );
     assertTrue( exception.getMessage().startsWith( "Unable to encode:" ) );
   }
@@ -275,19 +275,19 @@ public final class JsonEncoderTest
   @Test
   public void encodeLong()
   {
-    final int id = ValueUtil.randomInt();
-    final int typeID = 42;
-    final HashMap<String, Serializable> routingKeys = new HashMap<>();
-    final HashMap<String, Serializable> attributeData = new HashMap<>();
+    final var id = ValueUtil.randomInt();
+    final var typeID = 42;
+    final var routingKeys = new HashMap<String, Serializable>();
+    final var attributeData = new HashMap<String, Serializable>();
     attributeData.put( "X", 1392061102056L );
-    final EntityMessage message =
+    final var message =
       new EntityMessage( id, typeID, 0, routingKeys, attributeData, null );
-    final ChangeSet cs = new ChangeSet();
+    final var cs = new ChangeSet();
     cs.merge( new Change( message ) );
 
-    final String encoded = JsonEncoder.encodeChangeSet( null, null, null, cs );
+    final var encoded = JsonEncoder.encodeChangeSet( null, null, null, cs );
 
-    final String value =
+    final var value =
       toJsonObject( encoded ).
         getJsonArray( Messages.Update.CHANGES ).
         getJsonObject( 0 ).
@@ -300,8 +300,8 @@ public final class JsonEncoderTest
   @Test
   public void encodeUseCacheMessage()
   {
-    final ChannelAddress address = new ChannelAddress( 1, 2, "inst" );
-    final JsonObject message = toJsonObject( JsonEncoder.encodeUseCacheMessage( address, "e1", 7 ) );
+    final var address = new ChannelAddress( 1, 2, "inst" );
+    final var message = toJsonObject( JsonEncoder.encodeUseCacheMessage( address, "e1", 7 ) );
 
     assertEquals( message.getString( Messages.Common.TYPE ), Messages.S2C_Type.USE_CACHE );
     assertEquals( message.getString( Messages.Common.CHANNEL ), "1.2#inst" );
@@ -312,8 +312,8 @@ public final class JsonEncoderTest
   @Test
   public void encodeUseCacheMessage_withoutRequestId()
   {
-    final ChannelAddress address = new ChannelAddress( 1, 2 );
-    final JsonObject message = toJsonObject( JsonEncoder.encodeUseCacheMessage( address, "e1", null ) );
+    final var address = new ChannelAddress( 1, 2 );
+    final var message = toJsonObject( JsonEncoder.encodeUseCacheMessage( address, "e1", null ) );
 
     assertEquals( message.getString( Messages.Common.TYPE ), Messages.S2C_Type.USE_CACHE );
     assertEquals( message.getString( Messages.Common.CHANNEL ), "1.2" );
@@ -324,7 +324,7 @@ public final class JsonEncoderTest
   @Test
   public void encodeSessionCreatedMessage()
   {
-    final JsonObject message = toJsonObject( JsonEncoder.encodeSessionCreatedMessage( "sid-1" ) );
+    final var message = toJsonObject( JsonEncoder.encodeSessionCreatedMessage( "sid-1" ) );
 
     assertEquals( message.getString( Messages.Common.TYPE ), Messages.S2C_Type.SESSION_CREATED );
     assertEquals( message.getString( Messages.S2C_Common.SESSION_ID ), "sid-1" );
@@ -333,7 +333,7 @@ public final class JsonEncoderTest
   @Test
   public void encodeOkMessage()
   {
-    final JsonObject message = toJsonObject( JsonEncoder.encodeOkMessage( 4 ) );
+    final var message = toJsonObject( JsonEncoder.encodeOkMessage( 4 ) );
 
     assertEquals( message.getString( Messages.Common.TYPE ), Messages.S2C_Type.OK );
     assertEquals( message.getInt( Messages.Common.REQUEST_ID ), 4 );
@@ -342,7 +342,7 @@ public final class JsonEncoderTest
   @Test
   public void encodeMalformedMessageMessage()
   {
-    final JsonObject message = toJsonObject( JsonEncoder.encodeMalformedMessageMessage( "bad" ) );
+    final var message = toJsonObject( JsonEncoder.encodeMalformedMessageMessage( "bad" ) );
 
     assertEquals( message.getString( Messages.Common.TYPE ), Messages.S2C_Type.MALFORMED_MESSAGE );
     assertEquals( message.getString( Messages.S2C_Common.MESSAGE ), "bad" );
@@ -351,8 +351,8 @@ public final class JsonEncoderTest
   @Test
   public void encodeUnknownRequestType()
   {
-    final JsonObject command = Json.createObjectBuilder().add( "t", "x" ).build();
-    final JsonObject message = toJsonObject( JsonEncoder.encodeUnknownRequestType( command ) );
+    final var command = Json.createObjectBuilder().add( "t", "x" ).build();
+    final var message = toJsonObject( JsonEncoder.encodeUnknownRequestType( command ) );
 
     assertEquals( message.getString( Messages.Common.TYPE ), Messages.S2C_Type.UNKNOWN_REQUEST_TYPE );
     assertEquals( message.getJsonObject( Messages.Common.COMMAND ).toString(), command.toString() );
@@ -361,7 +361,7 @@ public final class JsonEncoderTest
   @Test
   public void encodeErrorMessage()
   {
-    final JsonObject message = toJsonObject( JsonEncoder.encodeErrorMessage( "oops" ) );
+    final var message = toJsonObject( JsonEncoder.encodeErrorMessage( "oops" ) );
 
     assertEquals( message.getString( Messages.Common.TYPE ), Messages.S2C_Type.ERROR );
     assertEquals( message.getString( Messages.S2C_Common.MESSAGE ), "oops" );
